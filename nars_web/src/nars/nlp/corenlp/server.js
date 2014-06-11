96e20801e539ee755463696b0b0586b22753cc34
sseehh <1s1e1h1@gmail.com>
2014-06-11 11:34:47 -0400

    cleanup & idiot-proofing

--- nars_web/src/nars/nlp/corenlp/server.js
+++ nars_web/src/nars/nlp/corenlp/server.js
@@ -1,74 +1,94 @@
+/* Simple Sanford CoreNLP Server
+ * 
+ * To setup a CoreNLP Server:
+ *  1. extract corenlp to a directory
+ *  2. edit nlp.sh setting the appropriate path to the 'java' binary and corenlp
+ *  3. run nlp.sh to make sure it works, reading from stdin and output to stdout
+ *  4. run this script which will pipe incoming socket connection to corenlp
+ *          nohup server.js &
+ *  
+ */
+
+var port = 9100;
+
 var net = require('net');
 var readline = require('readline');
 
-var spawn = require('child_process').spawn,
-    ls    = spawn('./nlp.sh');
+var spawn = require('child_process').spawn;
+var nlp = spawn('./nlp.sh');
 
-ls.stdout.on('data', function (data) {
-  console.log('stdout: ' + data);
+nlp.stdout.on('data', function(data) {
+    console.log('stdout: ' + data);
 });
 
-ls.stderr.on('data', function (data) {
-  console.log('stderr: ' + data);
+nlp.stderr.on('data', function(data) {
+    console.log('stderr: ' + data);
 });
 
-ls.on('close', function (code) {
-  console.log('child process exited with code ' + code);
+nlp.on('close', function(code) {
+    console.log('child process exited with code ' + code);
 });
 
 var server = net.createServer();
 //server.maxConnections = 1;
- 
-var port = 9100;
- 
+
+
 server.listen(port);
- 
+
 server.once('listening', function() {
-  console.log('Server listening on port %d', port);
+    console.log('Server listening on port %d', port);
 });
 
 var pending = [];
 var connected = false;
 
 server.on('connection', function(stream) {
-	var p = processConnection(stream);
-	if ((pending.length == 0) && (!connected)) p();
-	else {
-		pending.push(p);
-	}
+    var p = processConnection(stream);
+    if ((pending.length == 0) && (!connected))
+        p();
+    else {
+        pending.push(p);
+    }
 });
 
-//handle client's own disconnect
-server.on('close', function(stream) {
-	if ((connected) && (pending.length > 0)) {
-		pending.shift()();	
-	}
-});
+//handle client's own disconnect?
+/*server.on('close', function(stream) {
+    if ((connected) && (pending.length > 0)) {
+        pending.shift()();
+    }
+});*/
 
 function processConnection(stream) {
-return function() {
-  connected = true;
-  stream.pipe(ls.stdin);
-  ls.stdout.pipe(stream);
 
- var rl = readline.createInterface({
-  input: ls.stdout,
-  output: stream
- });
-  rl.on('line', function (cmd) {
-  if (cmd == '') {
-	stream.unpipe(ls.stdin);
-	  stream.end();
-	connected = false;
-	rl.close();
- if (pending.length > 0) {
-        var r = pending.shift();
-        r();
- }
+    return function() {
+        connected = true;
+        stream.pipe(nlp.stdin);
+        nlp.stdout.pipe(stream);
+        
+        var rl = readline.createInterface({
+            input: nlp.stdout,
+            output: stream
+        });
+        
+        function switchNext() {
+            stream.unpipe(nlp.stdin);
+            stream.end();
+            connected = false;
+            rl.close();
+            if (pending.length > 0) {
+                var r = pending.shift();
+                r();
+            }            
+        }
+        
+        rl.on('line', function(cmd) {
+            if (cmd == '') {
+                switchNext();
+            }
+        });
+        
+        rl.on('close', switchNext);
 
-  }
- });
-
-}
+    }
 }
 
