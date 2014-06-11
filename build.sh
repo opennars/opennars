b993c1a007d43ee76b58c825b7c1fa27dac3b4d3
sseehh <1s1e1h1@gmail.com>
2014-06-11 00:17:16 -0400

    build process includes nars_web

--- build.sh
+++ build.sh
@@ -4,11 +4,16 @@ rm -rf classes
 mkdir classes
 SRC=src/main/java
 SRC=""
-javac -d classes -Xlint:unchecked nars_gui/$SRC/*/*/*.java \
-	nars_java/$SRC/*/*/*.java -cp "build/junit-4.11.jar"
+javac -d classes -Xlint:unchecked \
+        nars_gui/$SRC/*/*/*.java \
+	nars_java/$SRC/*/*/*.java \
+        nars_web/src/*/*/*.java \
+        -cp "build/junit-4.11.jar:nars_web/lib/java_websocket.jar:nars_web/lib/gson-2.2.4.jar"
 
 # javac -d classes -Xlint:unchecked nars/*/*.java
 echo 'Main-Class: nars.main.NARS' > manifest.txt
+echo 'Class-Path: nars_web/lib/java_websocket.jar:nars_web/lib/gson-2.2.4.jar' >> manifest.txt
+
 jar cvfm NARS.jar manifest.txt -C classes . 
 rm manifest.txt
 
@@ -19,3 +24,5 @@ echo or
 echo 'java -jar NARS.jar nars-dist/Examples/Example-NAL1-edited.txt --silence 90 &'
 echo or
 echo 'java -cp NARS.jar nars.main_nogui.NARSBatch nars-dist/Examples/Example-NAL1-edited.txt'
+echo or
+echo 'java -cp "NARS.jar:nars_web/lib/*" nars.web.NARServer'
