4cf4c1ba7d0cf7feb2874ea4345f56940134bd51
sseehh <1s1e1h1@gmail.com>
2014-06-23 17:46:32 -0400

    updates

--- build.sh
+++ build.sh
@@ -1,21 +1,17 @@
 ant fulljar
 
 echo ''
-echo 'You can now launch:'
-echo 'java -jar dist/OpenNARS.jar &'
-echo or
-echo 'java -jar dist/OpenNARS.jar nars-dist/Examples/Example-NAL1-edited.txt --silence 90 &'
-echo or
-echo 'java -cp dist/OpenNARS.jar nars.main_nogui.NARSBatch nars-dist/Examples/Example-NAL1-edited.txt'
-echo or
-echo 'java -cp "dist/OpenNARS.jar:lib/*" nars.web.NARServer'
+echo 'You can now run:'
+echo '  Swing GUI: ./gui.sh'
+echo '  Web Server: ./web.sh'
+echo '  Console: ./nars.sh'
+echo '  Console input file: ./nars.sh [filename] [--silence <0..100>]'
 
-echo ''
-echo 'Set look & feel: -Dswing.defaultlaf=...'
-echo '  com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-echo '  com.sun.java.swing.plaf.windows.WindowsLookAndFeel'
-echo '  javax.swing.plaf.metal.MetalLookAndFeel'
-echo '  com.sun.java.swing.plaf.motif.MotifLookAndFeel'
+#echo 'Set look & feel: -Dswing.defaultlaf=...'
+#echo '  com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+#echo '  com.sun.java.swing.plaf.windows.WindowsLookAndFeel'
+#echo '  javax.swing.plaf.metal.MetalLookAndFeel'
+#echo '  com.sun.java.swing.plaf.motif.MotifLookAndFeel'
 
 
 
---
+++ gui.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+java -cp dist/OpenNARS.jar nars.gui.NARSwing $1 $2
+
