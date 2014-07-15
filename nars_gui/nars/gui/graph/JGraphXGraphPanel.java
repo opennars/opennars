a088bd6f511b9ab5c88c02cea936a90db43a5da6
sseehh <1s1e1h1@gmail.com>
2014-07-15 04:16:42 -0400

    processing 2d NARGraph visualization

--- nars_gui/nars/gui/graph/JGraphXGraphPanel.java
+++ nars_gui/nars/gui/graph/JGraphXGraphPanel.java
@@ -1,8 +1,11 @@
 package nars.gui.graph;
 
-import com.mxgraph.layout.mxCompactTreeLayout;
+import com.mxgraph.layout.mxFastOrganicLayout;
 import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.util.mxConstants;
 import java.awt.BorderLayout;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -17,20 +20,24 @@ import org.jgrapht.ext.JGraphXAdapter;
  *
  * https://github.com/jgrapht/jgrapht/blob/master/jgrapht-demo/src/main/java/org/jgrapht/demo/JGraphXAdapterDemo.java
  */
-public class GraphPanel extends JPanel {
+public class JGraphXGraphPanel extends JPanel {
     
     private JGraphXAdapter jgxAdapter;
     
 
-    public GraphPanel(NAR n) {
+    public JGraphXGraphPanel(NAR n) {
         super(new BorderLayout());
 
         
         NARGraph g = new NARGraph();
-        g.add(n, IncludeEverything, new NARGraph.DefaultGraphizer(false,false,false,true,true));        
+        g.add(n, IncludeEverything, new NARGraph.DefaultGraphizer(true,true,true,true,true));        
 
         // create a visualization using JGraph, via an adapter
-        jgxAdapter = new JGraphXAdapter(g);
+        jgxAdapter = new JGraphXAdapter(g) {
+
+
+          
+        };
         
 
         mxGraphComponent mxc = new mxGraphComponent(jgxAdapter) {
@@ -48,6 +55,14 @@ public class GraphPanel extends JPanel {
 
 
         
+        mxFastOrganicLayout layout = 
+                //new mxCompactTreeLayout(jgxAdapter);
+                new mxFastOrganicLayout(jgxAdapter);
+                //new mxCircleLayout(jgxAdapter);        
+        layout.setForceConstant(150);
+        layout.execute(jgxAdapter.getDefaultParent());
+        
+
         /*
         mxOrganicLayout layout = 
                 //new mxCompactTreeLayout(jgxAdapter);
@@ -55,12 +70,14 @@ public class GraphPanel extends JPanel {
                 //new mxCircleLayout(jgxAdapter);        
         layout.setEdgeLengthCostFactor(0.001);*/
         
+        
+        /*
         mxCompactTreeLayout layout = 
                 new mxCompactTreeLayout(jgxAdapter);
         
         layout.setLevelDistance(40);
         layout.setNodeDistance(50);
-        layout.execute(jgxAdapter.getDefaultParent());
+        layout.execute(jgxAdapter.getDefaultParent());*/
         
         
         jgxAdapter.setConnectableEdges(false);
@@ -72,18 +89,24 @@ public class GraphPanel extends JPanel {
     public static void main(String[] args) {
         NAR n = new NAR();
         
+        /*
         new TextInput(n, "<a --> b>.");
         new TextInput(n, "<b --> c>.");
         new TextInput(n, "<d <-> c>. %0.75;0.90%");
-        new TextInput(n, "<a --> c>?");                
-        
-        n.run(12);
-        
+        new TextInput(n, "<a --> c>?");      
         new TextInput(n, "<a --> d>?");
-
         n.run(12);
+        */
         
-        Window w = new Window("GraphPanel", new GraphPanel(n)) {
+        new TextInput(n, "<0 --> num>. %1.00;0.90% {0 : 1}");
+        new TextInput(n, "<<$1 --> num> ==> <(*,$1) --> num>>. %1.00;0.90% {0 : 2}"); 
+        new TextInput(n, "<(*,(*,(*,0))) --> num>?  {0 : 3}");
+       
+        n.run(220);
+        
+
+        
+        Window w = new Window("GraphPanel", new JGraphXGraphPanel(n)) {
 
             @Override           protected void close() {            }
             
