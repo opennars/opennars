0463cfabcf12232aab20c64d9081e772a1cd1138
sseehh <1s1e1h1@gmail.com>
2014-07-25 15:12:35 -0400

    stable again

--- nars_java/nars/core/NARRun.java
+++ nars_java/nars/core/NARRun.java
@@ -19,7 +19,6 @@
 package nars.core;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
