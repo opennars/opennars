372af3aca1c22b8c0b82c1ed9b4b80048d99a54b
sseehh <1s1e1h1@gmail.com>
2014-08-01 22:17:42 -0400

    display updates

---
+++ nars_gui/nars/gui/output/PPanel.java
@@ -0,0 +1,34 @@
+package nars.gui.output;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import processing.core.PApplet;
+
+/**
+ * Processing.org Panel
+ */
+abstract public class PPanel extends PApplet {
+
+    public PPanel() {
+        super();
+    }
+
+
+
+    public void setup() {
+        noLoop();
+        
+    }
+
+    
+
+    public JPanel newPanel() {
+        JPanel p = new JPanel(new BorderLayout());
+        p.add(this, BorderLayout.CENTER);
+        init();
+        return p;
+    }
+    
+
+}
+
