96dbc1a6473335071235c90262c41dc34edc2353
tc <tc@box>
2014-08-11 04:13:08 +0000

    NAL8-5 test cases, all passing. Full working NAL8?

--- nars_test/nars/test/core/NALTestSome.java
+++ nars_test/nars/test/core/NALTestSome.java
@@ -15,7 +15,7 @@ import org.junit.runners.Parameterized;
 public class NALTestSome extends NALTest {
 
    public static boolean include(String filename) {
-       return filename.startsWith("nal8.4");
+       return filename.startsWith("nal8.5");
    }
 
    
