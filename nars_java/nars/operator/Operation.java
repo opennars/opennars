ad44fc0afaa43153b327c862196f286d0ca9c800
tc <tc@box>
2014-08-25 01:34:18 +0000

    fixed a exception caused by operators, since they are inheritance and it can be valid to represent operator list as a variable, the only way like SeH suggested is to only use getSubject and getPredicate, and do operator calling and such only if the subject is indeed a Product and the Predicate is indeed a operator.

--- nars_java/nars/operator/Operation.java
+++ nars_java/nars/operator/Operation.java
@@ -88,6 +88,7 @@ public class Operation extends Inheritance {
         if (oper == null) {
             return null;
         }
+        
         CharSequence name = makeName(oper.name(), arg);
         Term t = memory.conceptTerm(name);
         if (t != null) {
@@ -112,12 +113,14 @@ public class Operation extends Inheritance {
 
     @Override
     protected CharSequence makeName() {
-        return makeName(getOperator().name(), getArguments());
+        if(getSubject() instanceof Product && getPredicate() instanceof Operator)
+            return makeName(getPredicate().name(), ((Product)getSubject()).term);
+        return makeStatementName(getSubject(), Symbols.NativeOperator.INHERITANCE, getPredicate());
     }
 
     
     public static CharSequence makeName(final CharSequence op, Term[] arg) {
-        final StringBuilder nameBuilder = new StringBuilder(16 /* estimate */)
+        final StringBuilder nameBuilder = new StringBuilder(16) //estimate
                 .append(COMPOUND_TERM_OPENER.ch).append(op);
         
         for (final Term t : arg) {
@@ -130,12 +133,12 @@ public class Operation extends Inheritance {
     }
     
     
-    public Operator getOperator() {
+    /*public Operator getOperator() {
         return (Operator) getPredicate();
     }
     
     public Term[] getArguments() {
         return ((CompoundTerm) getSubject()).term;
-    }    
+    }*/
     
 }
