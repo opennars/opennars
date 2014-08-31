85c6e1733392eef4ceeb7a67ffabdebd8564be67
sseehh <1s1e1h1@gmail.com>
2014-08-30 22:39:04 -0400

    working multi-log w/ checkbox

--- nars_gui/nars/gui/output/MultiLogPanel.java
+++ nars_gui/nars/gui/output/MultiLogPanel.java
@@ -1,14 +1,29 @@
 package nars.gui.output;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.HashMap;
 import java.util.Map;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import nars.core.NAR;
 import nars.entity.Task;
 import nars.gui.NARControls;
@@ -16,78 +31,195 @@ import nars.gui.output.SwingLogPanel.SwingLogText;
 import nars.io.Output;
 
 /**
- *
+ * TODO queue outputs in non-displayed SwingLogPanel's into ArrayDeque without involving
+ * any display methods
+ * 
  * @author me
  */
-public class MultiLogPanel extends JPanel implements Output {
+public class MultiLogPanel extends JSplitPane implements Output {
 
-    public Map<Task, SwingLogText> taskPanel = new HashMap();
+    public class SwingLogTextM extends SwingLogText  implements ChangeListener {
+    
+        
+        public final JCheckBox enabler;
+        private final Object category;
+        JPanel displayed = null;
+
+        public SwingLogTextM(Object category) {
+            this.category = category;
+            this.enabler = new JCheckBox(category.toString());
+            
+            enabler.addChangeListener(this);
+            
+        }
+
+        @Override
+        public void stateChanged(ChangeEvent e) {
+            if (enabler.isSelected()) {
+                displayed = showCategory(category);                
+            }
+            else {                
+                hideCategory(displayed);
+                
+                displayed = null;
+            }
+        }
+
+        public void show() {
+            enabler.setSelected(true);
+        }
+    }
+    
+    public Map<Object, SwingLogTextM> categories = new HashMap();
     private final SwingLogText rootTaskPanel;
     private final NAR nar;
     private final JPanel content;
+    private final JPanel side;
+    private final DefaultListModel categoriesListModel;
+    private final JCheckBoxList categoriesList;
 
     public MultiLogPanel(NARControls c) {
-        super(new BorderLayout());
+        super(JSplitPane.HORIZONTAL_SPLIT);
         
+        categoriesListModel = new DefaultListModel();
+        categoriesList = new JCheckBoxList(categoriesListModel);
+        
+
+        side = new JPanel(new BorderLayout());
+        side.add(categoriesList, BorderLayout.CENTER);
+        add(new JScrollPane(side), 0);
+
         content = new JPanel(new GridLayout(1, 0));
-        add(content, BorderLayout.CENTER);
+        add(new JScrollPane(content, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED), 1);
+
+        setDividerLocation(0.14);
 
         this.nar = c.nar;
         nar.addOutput(this);
-        
+
         rootTaskPanel = new SwingLogText() {
 
         };
-        add("Root", rootTaskPanel);
+        
+        
+        getLogPanel("Root").show();
     }
 
     @Override
     public void output(Class channel, Object o) {
+        Object category;
         if (o instanceof Task) {
             Task t = (Task) o;
-            Task parent = t.getRootTask();
-            SwingLogText p = getLogPanel(parent);
-            p.print(channel, o, false, nar);
+            category = t.getRootTask();
         } else {
-            rootTaskPanel.print(channel, o, false, nar);
+            category = null;
         }
+        
+        SwingLogText p = getLogPanel(category);
+        if (p!=null)
+            p.print(channel, o, false, nar);
     }
 
-
-    SwingLogText getLogPanel(Task parent) {
-        if (parent == null) {
-            return rootTaskPanel;
+    public SwingLogText getLogPanel(Object category) {
+        if (category == null) {
+            return categories.get("Root");
         } else {
-            SwingLogText p = taskPanel.get(parent);
+            SwingLogTextM p = categories.get(category);
             if (p == null) {
-                p = new SwingLogText();
-                add(parent.toStringBrief(), p);
-                taskPanel.put(parent, p);
+                
+                p = new SwingLogTextM(category);
+                JCheckBox jc = p.enabler;
+                
+                categories.put(category, p);
+                                        
+                categoriesListModel.addElement(jc);
             }
             return p;
         }
     }
-    
-    public void add(String title, SwingLogText p) {
+
+    public JPanel showCategory(Object category) {
+        String title = category.toString();
+        SwingLogText p = getLogPanel(category);
         int columnWidth = 400;
         SwingLogPanel.setConsoleStyle(p, true);
-                
+
         JPanel x = new JPanel(new BorderLayout());
         x.add(new JButton(title), BorderLayout.NORTH);
         x.add(new JScrollPane(p), BorderLayout.CENTER);
-        
-        
+
         x.setMinimumSize(new Dimension(columnWidth, 0));
-        x.setMaximumSize(new Dimension(columnWidth, Integer.MAX_VALUE/2));
+        x.setMaximumSize(new Dimension(columnWidth, Integer.MAX_VALUE / 2));
         x.setPreferredSize(new Dimension(columnWidth, 0));
         x.validate();
-        
+
         content.add(x);
+        
         SwingUtilities.invokeLater(new Runnable() {
-            @Override public void run() {
+            @Override
+            public void run() {
                 revalidate();
                 repaint();
-            }            
+            }
         });
+        
+        return x;
+    }
+
+    /**
+     * @author http://stackoverflow.com/questions/19766/how-do-i-make-a-list-with-checkboxes-in-java-swing
+     */
+    @SuppressWarnings("serial")
+    public static class JCheckBoxList extends JList<JCheckBox> {
+
+        protected static Border noFocusBorder = new EmptyBorder(1, 1, 1, 1);
+
+        public JCheckBoxList() {
+            setCellRenderer(new CellRenderer());
+            addMouseListener(new MouseAdapter() {
+                public void mousePressed(MouseEvent e) {
+                    int index = locationToIndex(e.getPoint());
+                    if (index != -1) {
+                        JCheckBox checkbox = (JCheckBox) getModel().getElementAt(index);
+                        checkbox.setSelected(!checkbox.isSelected());
+                        repaint();
+                    }
+                }
+            });
+            setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        }
+
+        public JCheckBoxList(ListModel<JCheckBox> model) {
+            this();
+            setModel(model);
+        }
+
+        protected class CellRenderer implements ListCellRenderer<JCheckBox> {
+
+            public Component getListCellRendererComponent(
+                    JList<? extends JCheckBox> list, JCheckBox value, int index,
+                    boolean isSelected, boolean cellHasFocus) {
+                JCheckBox checkbox = value;
+
+                //Drawing checkbox, change the appearance here
+                checkbox.setBackground(isSelected ? getSelectionBackground()
+                        : getBackground());
+                checkbox.setForeground(isSelected ? getSelectionForeground()
+                        : getForeground());
+                checkbox.setEnabled(isEnabled());
+                checkbox.setFont(getFont());
+                checkbox.setFocusPainted(false);
+                checkbox.setBorderPainted(true);
+                checkbox.setBorder(isSelected ? UIManager
+                        .getBorder("List.focusCellHighlightBorder") : noFocusBorder);
+                return checkbox;
+            }
+        }
+    }
+    
+    public void hideCategory(JPanel p) {
+        content.remove(p);
+        validate();
+        repaint();
     }
 }
