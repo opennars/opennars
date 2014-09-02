c0e2725547fdef84ba5b7423afef531dd86e3004
sseehh <1s1e1h1@gmail.com>
2014-09-02 19:42:41 -0400

    live syntax verifier in InputPanel

--- nars_gui/nars/gui/input/InputPanel.java
+++ nars_gui/nars/gui/input/InputPanel.java
@@ -33,10 +33,15 @@ import java.io.File;
 import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.JTree;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 import nars.core.NAR;
@@ -45,7 +50,11 @@ import nars.gui.NPanel;
 import static nars.gui.output.SwingLogPanel.setConsoleStyle;
 import nars.io.Output.OUT;
 import nars.io.TextInput;
-
+import nars.io.narsese.NarseseParser;
+import org.parboiled.errors.InvalidInputError;
+import org.parboiled.parserunners.ReportingParseRunner;
+import org.parboiled.support.MatcherPath;
+import org.parboiled.support.ParsingResult;
 
 public class InputPanel extends NPanel implements ActionListener {
 
@@ -57,10 +66,10 @@ public class InputPanel extends NPanel implements ActionListener {
     /**
      * Input area
      */
-    private final JTextArea inputText;
+    private JTextArea inputText;
     /**
      * Whether the window is ready to accept new addInput (in fact whether the
- Reasoner will read the content of {@link #inputText} )
+     * Reasoner will read the content of {@link #inputText} )
      */
     private boolean ready;
     /**
@@ -69,6 +78,7 @@ public class InputPanel extends NPanel implements ActionListener {
     private int timer;
     private final JPanel centerPanel;
     private final JTree fileTree;
+    private final JComponent narseseInput;
 
     /**
      * Constructor
@@ -78,42 +88,40 @@ public class InputPanel extends NPanel implements ActionListener {
      */
     public InputPanel(final NAR nar) {
         super(new BorderLayout());
-        
+
         centerPanel = new JPanel(new BorderLayout());
-                
+
         JPanel menu = new JPanel();
-        menu.setLayout(new FlowLayout(FlowLayout.RIGHT,0,0));
+        menu.setLayout(new FlowLayout(FlowLayout.RIGHT, 0, 0));
 
         menu.setOpaque(false);
         setBackground(Color.BLACK);
-        
-
 
         final JComboBox modeSelect = new JComboBox();
-        modeSelect.addItem("Text");
+        modeSelect.addItem("Narsese");
         modeSelect.addItem("Files");
         modeSelect.addActionListener(new ActionListener() {
-            @Override public void actionPerformed(ActionEvent e) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 updateMode(modeSelect.getSelectedIndex());
             }
         });
         menu.add(modeSelect);
-                
+
         eval = new JButton("Evaluate");
         eval.setDefaultCapable(true);
         eval.setToolTipText("Input the text, each line executed in successive clock cycles.");
         eval.addActionListener(this);
         menu.add(eval);
-        
+
         /*
-        evalAll = new JButton("Eval All");
-        evalAll.setDefaultCapable(true);
-        evalAll.addActionListener(this);
-        evalAll.setToolTipText("Input the text, each line executed in the same clock cycle.  (Ctrl-enter)");                
-        evalAll.addActionListener(this);
-        menu.add(evalAll);
-        */
-        
+         evalAll = new JButton("Eval All");
+         evalAll.setDefaultCapable(true);
+         evalAll.addActionListener(this);
+         evalAll.setToolTipText("Input the text, each line executed in the same clock cycle.  (Ctrl-enter)");                
+         evalAll.addActionListener(this);
+         menu.add(evalAll);
+         */
         holdButton = new JButton("Hold");
         holdButton.addActionListener(this);
         menu.add(holdButton);
@@ -121,7 +129,7 @@ public class InputPanel extends NPanel implements ActionListener {
         clearButton = new JButton("Clear");
         clearButton.addActionListener(this);
         menu.add(clearButton);
-        
+
         TreeModel model = new FileTreeModel(new File("./nal"));
         fileTree = new JTree(model);
         fileTree.addMouseListener(new MouseAdapter() {
@@ -148,38 +156,143 @@ public class InputPanel extends NPanel implements ActionListener {
             }
         });
 
-        inputText = new JTextArea("");
-        inputText.setRows(3);        
-        inputText.addKeyListener(new KeyAdapter() {
-            @Override public void keyReleased(KeyEvent e) {
-                //control-enter evaluates
-                if (e.isControlDown())
-                    if (e.getKeyCode()==10) {
-                        eval.doClick();
-                    }
-            }           
-        });
-        setConsoleStyle(inputText, true, 19);
+        narseseInput = newNarseseInput();
 
         updateMode(0);
-        
+
         add(centerPanel, BorderLayout.CENTER);
         add(menu, BorderLayout.SOUTH);
-        
-        
+
         this.reasoner = nar;
     }
 
     private void updateMode(int selectedIndex) {
         centerPanel.removeAll();
         if (selectedIndex == 0) {
-            centerPanel.add(inputText, BorderLayout.CENTER);
-        }
-        else if (selectedIndex == 1) {
+            centerPanel.add(narseseInput, BorderLayout.CENTER);
+        } else if (selectedIndex == 1) {
             centerPanel.add(new JScrollPane(fileTree), BorderLayout.CENTER);
-        }        
+        }
         centerPanel.validate();
         repaint();
+    }
+
+    public static class NarseseParsePanel extends JPanel {
+
+        /**
+         * can be static?
+         */
+        public final NarseseParser p = NarseseParser.newParser();
+        public final JTextPane info;
+
+        public NarseseParsePanel() {
+            super(new BorderLayout());
+
+            info = new JTextPane();
+            info.setEditable(false);
+            add(new JScrollPane(info), BorderLayout.CENTER);
+        }
+
+        public boolean update(String input) {
+
+            ReportingParseRunner rpr = new ReportingParseRunner(p.Task());
+            ParsingResult r = rpr.run(input);
+
+            String s = "";
+            boolean valid = (r.parseErrors.size() == 0);
+            if (!valid) {
+                for (Object e : r.parseErrors) {
+                    if (e instanceof InvalidInputError) {
+                        InvalidInputError iie = (InvalidInputError) e;
+                        s += iie.getClass().getSimpleName() + " " + iie.getErrorMessage() + "\n";
+                        s += (" at: " + iie.getStartIndex() + " to " + iie.getEndIndex()) + "\n";
+
+                        for (MatcherPath m : iie.getFailedMatchers()) {
+                            s += ("  ?-> " + m + '\n');
+                        }
+                    } else {
+                        s += e.toString();
+                    }
+                }
+            } else {
+                s = "OK. ";
+            }
+            info.setText(s);
+
+            return valid;
+
+        }
+
+    }
+
+    //TODO move this to its own class
+    public JComponent newNarseseInput() {
+        JSplitPane p = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+
+        NarseseParsePanel infoPane = new NarseseParsePanel();
+
+        setConsoleStyle(infoPane.info, true, 16);
+
+        inputText = new JTextArea("");
+        inputText.setRows(3);
+
+        DocumentListener documentListener = new DocumentListener() {
+            public void changedUpdate(DocumentEvent documentEvent) {
+                printIt(documentEvent);
+            }
+
+            public void insertUpdate(DocumentEvent documentEvent) {
+                printIt(documentEvent);
+            }
+
+            public void removeUpdate(DocumentEvent documentEvent) {
+                printIt(documentEvent);
+            }
+
+            private void printIt(DocumentEvent documentEvent) {
+                String t = inputText.getText();
+                
+                boolean valid = true, show;
+                if (t.length() > 0) {
+                    valid = infoPane.update(t);
+                    show = !valid;
+                    
+                }
+                else {
+                    show = false;
+                }
+                
+                if (show) {
+                    infoPane.setVisible(true);
+                    p.setDividerLocation(0.5f);
+                } else {
+                    infoPane.setVisible(false);
+                    p.setDividerLocation(1.0f);
+                }
+            }
+        };
+        inputText.getDocument().addDocumentListener(documentListener);
+
+        inputText.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                //control-enter evaluates
+                if (e.isControlDown()) {
+                    if (e.getKeyCode() == 10) {
+                        eval.doClick();
+                    }
+                }
+            }
+        });
+        setConsoleStyle(inputText, true, 19);
+
+        p.add(inputText, 0);
+
+        infoPane.setVisible(false);
+        p.add(infoPane, 1);
+        p.setDividerLocation(1.0f);
+
+        return p;
     }
 
     /**
@@ -193,13 +306,12 @@ public class InputPanel extends NPanel implements ActionListener {
     @Override
     protected void onShowing(boolean showing) {
         if (showing) {
-            
-        }
-        else {
-        
+
+        } else {
+
         }
     }
-    
+
     /**
      * Handling button click
      *
@@ -227,17 +339,16 @@ public class InputPanel extends NPanel implements ActionListener {
         reasoner.addInput(input);
         reasoner.step(1);
     }
+
     public void evaluateSeq(String input) {
         //TODO make sequential evaluation
         reasoner.addInput(input);
         reasoner.step(1);
     }
-   
+
     private void close() {
         setVisible(false);
     }
-
-    
 
 //    /**
 //     * Accept text addInput in a tick, which can be multiple lines TODO some
@@ -277,5 +388,4 @@ public class InputPanel extends NPanel implements ActionListener {
 //        return ((text.length() > 0) || (timer > 0));
 //    }
 //
-  
 }
