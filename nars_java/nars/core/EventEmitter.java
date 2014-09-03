fe7649066307e385940ab4a039d3a7a25335fd85
sseehh <1s1e1h1@gmail.com>
2014-09-03 15:39:58 -0400

    swing threading update

--- nars_java/nars/core/EventEmitter.java
+++ nars_java/nars/core/EventEmitter.java
@@ -1,10 +1,10 @@
 
 package nars.core;
 
-import java.util.concurrent.BlockingQueue;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Adapted from http://www.recursiverobot.com/post/86215392884/witness-a-simple-android-and-java-event-emitter
@@ -15,12 +15,11 @@ public class EventEmitter {
         public void event(Class event, Object... arguments);
     }
 
-    private final ConcurrentMap<Class<?>, ConcurrentMap<Observer, String>> events
-            = new ConcurrentHashMap<Class<?>, ConcurrentMap<Observer, String>>();
+    private final Map<Class<?>, List<Observer>> events
+            = new ConcurrentHashMap<Class<?>, List<Observer>>();
  
-    private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
+    //private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
     //private final ExecutorService executorService = new ThreadPoolExecutor(1, 10, 30, TimeUnit.SECONDS, queue);
- 
 
     public boolean hasAnyOn(final Class event) {
         if (events.get(event)!=null)
@@ -32,9 +31,15 @@ public class EventEmitter {
     public <C> void on(final Class<? extends C> event, final Observer<? extends C> o) {
         if (null == event || null == o)
             return;
- 
-        events.putIfAbsent(event, new ConcurrentHashMap<Observer, String>());
-        events.get(event).putIfAbsent(o, "");
+         
+        if (events.containsKey(event))
+            events.get(event).add(o);
+        else {
+            ArrayList a = new ArrayList<Observer>(4);
+            a.add(o);
+            events.put(event, a);
+        }
+                
     }
  
     public void off(final Class<?> event, final Observer o) {
@@ -43,7 +48,7 @@ public class EventEmitter {
  
         if (!events.containsKey(event))
             return;
- 
+        
         events.get(event).remove(o);
     }
  
@@ -51,22 +56,15 @@ public class EventEmitter {
     public void emit(final Class eventClass, final Object... params) {
         if (events.get(eventClass)==null) return;
         
-        ConcurrentMap<Observer, String> observers = events.get(eventClass);
+        List<Observer> observers = events.get(eventClass);
         
+        if (observers == null) return;
         if (observers.size() == 0) return;
-        
-        
-        if (!events.containsKey(eventClass))
-            return;
 
-        for (final Observer m : events.get(eventClass).keySet()) {
+        int n = observers.size();
+        for (int i = 0; i < n; i++) {
+            final Observer m = observers.get(i);
             m.event(eventClass, params);
-
-            /*executorService.execute(new Runnable() {
-                @Override public void run() {
-                    m.event(eventClass, params);
-                }
-            });*/
         }
     }
  
