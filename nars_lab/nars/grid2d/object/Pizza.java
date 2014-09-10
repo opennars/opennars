0e7d2ecde875dd90ec59bd83c6a0bbc4a755c6fd
tc <tc@box>
2014-09-10 00:31:47 +0000

    pizza drawing (from Prolog1)

--- nars_lab/nars/grid2d/object/Pizza.java
+++ nars_lab/nars/grid2d/object/Pizza.java
@@ -27,29 +27,35 @@ public class Pizza extends LocalGridObject {
     public void draw() {
         cx = (cx * (1.0f - animationLerpRate)) + (x * animationLerpRate);
         cy = (cy * (1.0f - animationLerpRate)) + (y * animationLerpRate);
-        cheading = (cheading * (1.0f - animationLerpRate/2.0f)) + (heading * animationLerpRate/2.0f);
-        
-        float scale = (float)Math.sin(space.getTime()/7f)*0.05f + 1.0f;
-        
+        cheading = (cheading * (1.0f - animationLerpRate / 2.0f)) + (heading * animationLerpRate / 2.0f);
+        float scale = (float) Math.sin(space.getTime() / 7f) * 0.05f + 1.0f;
         space.pushMatrix();
         space.translate(cx, cy);
-        
         space.pushMatrix();
-        space.scale(scale*0.8f);
+        space.scale(scale * 0.8f);
+        space.fill(Color.ORANGE.getRGB(), 255);
+        space.ellipse(0, 0, 1.0f, 1.0f);
         space.fill(Color.YELLOW.getRGB(), 255);
-        space.ellipse(0,0, 1, 1);
-        space.popMatrix();
+        space.ellipse(0, 0, 0.8f, 0.8f);
         
-        if(!"".equals(doorname))
-        {
+        space.popMatrix();
+        if (!"".equals(doorname)) {
             space.textSize(0.2f);
-            space.fill(255,0,0);
+            space.fill(255, 0, 0);
             space.pushMatrix();
-            space.text(doorname,0,0);
+            space.text(doorname, 0, 0);
             space.popMatrix();
         }
         
-        space.popMatrix();
+        //eyes
+        space.fill(Color.RED.getRGB(), 255);
+        space.rotate((float)(Math.PI/180f * cheading));
+        space.ellipse(-0.15f,0.2f,0.1f,0.1f);
+        space.ellipse(0.15f,0.2f,0.1f,0.1f);
+        space.ellipse(-0.2f,-0.2f,0.1f,0.1f);
+        space.ellipse(0.2f,-0.2f,0.1f,0.1f);
+        space.ellipse(0.0f,-0.0f,0.1f,0.1f);
         
+        space.popMatrix();
     }
 }
