96f1b0b64bdb110cd873171b6033fbba557a2570
sseehh <1s1e1h1@gmail.com>
2014-10-13 01:15:05 -0400

    updates

--- nars_gui/nars/gui/output/graph/ImplicationGraphCanvas.java
+++ nars_gui/nars/gui/output/graph/ImplicationGraphCanvas.java
@@ -42,7 +42,7 @@ public class ImplicationGraphCanvas extends AnimatedProcessingGraphCanvas<Term,C
 
     @Override
     public int getEdgeColor(Cause c) {
-        float a = (float)graphExec.getSentenceRelevancy(c);
+        float a = (float)graphExec.getCauseRelevancy(c);
         if (a > 1.0f) a = 1.0f;
         if (a < 0f) a = 0f;
 
@@ -57,7 +57,7 @@ public class ImplicationGraphCanvas extends AnimatedProcessingGraphCanvas<Term,C
     @Override
     public float getEdgeThickness(Cause c, VertexDisplay source, VertexDisplay target) {
         
-        float ww = (float)graphExec.getSentenceRelevancy(c);
+        float ww = (float)graphExec.getCauseRelevancy(c);
         if (ww < 1.0f) ww = 1.0f;
         
         
