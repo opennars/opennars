d1203924df66433dd176e7bee7c2aec7742b8e0c
tc <tc@box>
2014-10-14 12:18:28 +0000

    param adjustment, full internal experience plugin working again, distance check allowed for rover.

--- nars_java/nars/inference/Executive.java
+++ nars_java/nars/inference/Executive.java
@@ -574,7 +574,7 @@ public class Executive {
     
     //is input or by the system triggered operation
     public boolean isInputOrTriggeredOperation(final Task newEvent, Memory mem) {
-        if(!((newEvent.isInput()) || (newEvent.getCause()!=null))) {
+        if(!((newEvent.isInput() || Parameters.INTERNAL_EXPERIENCE_FULL) || (newEvent.getCause()!=null))) {
             return false;
         }
         /*Term newcontent=newEvent.sentence.content;
