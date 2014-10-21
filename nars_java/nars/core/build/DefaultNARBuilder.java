5687d53cde94efbeb6b7665f6a7eb85b169c7ab2
sseehh <1s1e1h1@gmail.com>
2014-10-21 15:35:07 -0400

    updates

--- nars_java/nars/core/build/DefaultNARBuilder.java
+++ nars_java/nars/core/build/DefaultNARBuilder.java
@@ -79,6 +79,7 @@ public class DefaultNARBuilder extends NARBuilder implements ConceptBuilder {
         p.newTaskForgetDurations.set(2.0);
                 
         p.conceptBeliefsMax.set(7);
+        p.conceptGoalsMax.set(7);
         p.conceptQuestionsMax.set(5);
         
         p.contrapositionPriority.set(30);
