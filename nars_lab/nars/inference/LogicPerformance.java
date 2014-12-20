99ebeaadc998731e1160986284654aa828d290ae
SeH <1s1e1h1@gmail.com>
2014-12-19 22:14:25 -0500

    calculation of non-essential inference steps by connectivity to solution task

--- nars_lab/nars/inference/LogicPerformance.java
+++ nars_lab/nars/inference/LogicPerformance.java
@@ -5,6 +5,7 @@
 package nars.inference;
 
 import automenta.vivisect.swing.NWindow;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
@@ -29,12 +30,17 @@ import nars.operator.Operation;
 import nars.util.NARGraph;
 import nars.util.NARGraph.TimeNode;
 import nars.util.NARGraph.UniqueEdge;
+import org.jgrapht.alg.DijkstraShortestPath;
 
 /**
  * Graph analysis of reasoning processes to determine essential and non-essential
  * activity
+ * 
+ * @see http://jgrapht.org/javadoc/org/jgrapht/alg/package-summary.html
  */
 public class LogicPerformance {
+    private final TaskReasonGraph essential;
+    private final Collection<Task> solutionTask;
     
     /**
      * contains tasks/sentences, time cycles, etc to explain a task's formation */     
@@ -146,7 +152,45 @@ public class LogicPerformance {
     public LogicPerformance(TaskReasonGraph process, Collection<Task> solutionTasks) {
         Set<TimeNode> allCycles = process.vertices(TimeNode.class);        
      
-        System.out.println("Cycles: " + allCycles);
+        System.out.println("Time Cycles: " + allCycles);
+        
+        solutionTask = solutionTasks;
+        
+        //remove all tasks which have no path to a solution
+        System.out.println("Calculating essentials (" + process.vertexSet().size() + " vertices)");
+
+        essential = (TaskReasonGraph) process.clone();
+
+        List<Object> removeableVertex = new ArrayList();
+        
+        for (Object o : process.vertexSet()) {
+            if (o instanceof Task) {
+                Task t = (Task)o;
+                if (solutionTasks.contains(t))
+                    continue;
+                if (!connectsAny(t, solutionTasks))
+                    removeableVertex.add(t);                    
+            }
+        }
+        
+        System.out.println("Non-essential: ");
+        for (Object o : removeableVertex) {
+            essential.removeVertex(o);
+            System.out.println("  " + o + " (" + o.getClass().getSimpleName() +")");
+        }
+        
+        System.out.println(essential);
+        System.out.println(process.vertexSet().size() + " -> " + essential.vertexSet().size());
+        
+    }
+    
+    public boolean connectsAny(Object source, Iterable<? extends Object> targets) {
+        for (Object o : targets) {
+            DijkstraShortestPath d = new DijkstraShortestPath<>(essential, source, o);
+            if (d.getPath()!=null)
+                return true;            
+        }
+        return false;
     }
 
     
