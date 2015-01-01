4cd02f891fad3e15d5d7ebb78aa8fa5a04305316
SeH <1s1e1h1@gmail.com>
2015-01-01 17:08:33 -0500

    prediction demo gui

--- nars_java/nars/io/ChangedTextInput.java
+++ nars_java/nars/io/ChangedTextInput.java
@@ -8,6 +8,7 @@ public class ChangedTextInput  {
 
     private final NAR nar;
     private String last = null;
+    private boolean allowRepeats = false;
 
     public ChangedTextInput(NAR n) {
         this.nar = n;
@@ -18,13 +19,17 @@ public class ChangedTextInput  {
     }
     
     public boolean set(String s, long time) {
-        if ((last == null) || (!last.equals(s))) {
+        if (allowRepeats || (last == null) || (!last.equals(s))) {
             nar.addInput(s, time);
             last = s;
             return true;
         }
         //TODO option to, when else, add with lower budget ?
         return false;
+    }
+
+    public void setAllowRepeatInputs(boolean b) {
+        this.allowRepeats = b;
     }
 }
 
