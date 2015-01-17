b616d3b3182a61a6d023338caebea9b4227a023f
tc <tc@box>
2015-01-17 07:41:57 +0000

    Update ConsolePanel to 1.6.1 state (commit ed64c0e98fe04b8d163bb462fcefc22537d19fc7)

--- nars_gui/src/main/java/nars/gui/ConsolePanel.java
+++ nars_gui/src/main/java/nars/gui/ConsolePanel.java
@@ -1,12 +1,12 @@
 package nars.gui;
 
+import java.awt.Graphics;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
 import nars.gui.input.TextInputPanel;
 import nars.gui.output.LogPanel;
 import nars.gui.output.SwingLogPanel;
-
-import javax.swing.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Combines input panel with a log output panel, divided by a splitpane
@@ -21,23 +21,17 @@ public class ConsolePanel extends JSplitPane {
         
         TextInputPanel inputPanel = new TextInputPanel(narControls.nar);
         add(inputPanel, 1);
-        
-        SwingUtilities.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(ConsolePanel.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                setDividerLocation(0.75);
-            }
-            
-        });
-        
-        setDividerLocation(0.75);
-        
+    }
+    
+    int cnt=0;
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+   
+        if(cnt<5) { //according to stack overflow, the paint method is the only way to do this correctly
+            cnt++; //but it seems still not be guranteed 
+            this.setDividerLocation(0.75); //strangely on second paint it works, to make sure I put it to 5
+        }
     }
     
 }
