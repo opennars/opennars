4f59666708c5c27e6431ef8e549aabd556e3261c
tc <tc@box>
2015-01-17 07:22:41 +0000

    Update Anticipation Mechanism to 1.6.1 state (commit 4453e549274bb0d610119f2d95c09a99c6038184)

--- nars_java/src/main/java/nars/operator/mental/Anticipate.java
+++ nars_java/src/main/java/nars/operator/mental/Anticipate.java
@@ -22,38 +22,37 @@
 
 package nars.operator.mental;
 
+
+import java.util.*;
 import nars.core.Events;
 import nars.core.Events.CycleEnd;
 import nars.core.Memory;
 import nars.core.NAR;
 import nars.core.Parameters;
-import nars.io.Symbols;
 import nars.event.AbstractReaction;
+import nars.io.Symbols;
 import nars.logic.BudgetFunctions;
 import nars.logic.NAL;
 import nars.logic.entity.*;
 import nars.logic.nal4.Product;
 import nars.logic.nal5.Conjunction;
+import nars.logic.nal7.Interval;
 import nars.logic.nal7.TemporalRules;
 import nars.logic.nal8.Operation;
 import nars.logic.nal8.Operator;
-
-import java.util.*;
 
 /**
 * Operator that creates a judgment with a given statement
  */
 public class Anticipate extends Operator implements Mental {
 
-    public final Map<Long,LinkedHashSet<Term>> anticipations = new LinkedHashMap();
+    public final Map<Vector2Int,LinkedHashSet<Term>> anticipations = new LinkedHashMap();
             
     final Set<Term> newTasks = new LinkedHashSet();
     NAL nal;
 
     final static TruthValue expiredTruth = new TruthValue(0.0f, Parameters.DEFAULT_JUDGMENT_CONFIDENCE);
     final static BudgetValue expiredBudget = new BudgetValue(Parameters.DEFAULT_JUDGMENT_PRIORITY, Parameters.DEFAULT_JUDGMENT_DURABILITY, BudgetFunctions.truthToQuality(expiredTruth));
-    
-    transient private int duration;
     private AbstractReaction observer;
 
     public Anticipate() { super("^anticipate");     }
@@ -72,28 +71,52 @@ public class Anticipate extends Operator implements Mental {
         return true;
     }
     
+    class Vector2Int {
+        public long predictionCreationTime; //2014 and this is still the best way to define a data structure that simple?
+        public long predictedOccurenceTime; 
+        public Vector2Int(long predictionCreationTime, long predictedOccurenceTime) { //rest of the crap:
+            this.predictionCreationTime=predictionCreationTime; //when the prediction happened
+            this.predictedOccurenceTime=predictedOccurenceTime; //when the event is expected
+        }
+    }
+    
     public void updateAnticipations() {
 
         if (anticipations.isEmpty()) return;
 
         long now=nal.memory.time();
-        
-        this.duration = nal.memory.getDuration()*5;
-        long hopeExpirationWindow = duration;
                 
         //share stamps created by tasks in this cycle
         
         boolean hasNewTasks = !newTasks.isEmpty();
         
-        Iterator<Map.Entry<Long, LinkedHashSet<Term>>> aei = anticipations.entrySet().iterator();
+        Iterator<Map.Entry<Vector2Int, LinkedHashSet<Term>>> aei = anticipations.entrySet().iterator();
         while (aei.hasNext()) {
             
-            Map.Entry<Long, LinkedHashSet<Term>> ae = aei.next();
+            Map.Entry<Vector2Int, LinkedHashSet<Term>> ae = aei.next();
             
-            long aTime = ae.getKey();
-
-            boolean didntHappen = (now-aTime > hopeExpirationWindow);
-            boolean maybeHappened = hasNewTasks && Math.abs(aTime - now) <= duration;
+            long aTime = ae.getKey().predictedOccurenceTime;
+            long predictionstarted=ae.getKey().predictionCreationTime;
+            if(aTime < predictionstarted) { //its about the past..
+                return;
+            }
+            
+            //lets say a and <(&/,a,+4) =/> b> leaded to prediction of b with specific occurence time
+            //this indicates that this interval can be reconstructed by looking by when the prediction
+            //happened and for what time it predicted, Only when the happening would already lead to <(&/,a,+5) =/> b>
+            //we are allowed to apply CWA already, i think this is the perfect time to do this
+            //since there is no way anymore that the observation would support <(&/,a,+4) =/> b> at this time,
+            //also this way it is not applied to early, it seems to be the perfect time to me,
+            //making hopeExpirationWindow parameter entirely osbolete
+            Interval Int=Interval.interval(aTime-predictionstarted, nal.memory);
+            //ok we know the magnitude now, let's now construct a interval with magnitude one higher
+            //(this we can skip because magnitudeToTime allows it without being explicitly constructed)
+            //ok, and what predicted occurence time would that be? because only if now is bigger or equal, didnt happen is true
+            double expiredate=predictionstarted+Interval.magnitudeToTime(Int.magnitude+1, nal.memory.param.duration);
+            //
+            
+            boolean didntHappen = (now>=expiredate);
+            boolean maybeHappened = hasNewTasks && !didntHappen;
                 
             if ((!didntHappen) && (!maybeHappened))
                 continue;
@@ -184,7 +207,7 @@ public class Anticipate extends Operator implements Mental {
         LinkedHashSet<Term> ae = anticipations.get(occurenceTime);
         if (ae == null) {
             ae = new LinkedHashSet();
-            anticipations.put(occurenceTime, ae);
+            anticipations.put(new Vector2Int(memory.time(),occurenceTime), ae);
         }
         ae.add(content);
         
