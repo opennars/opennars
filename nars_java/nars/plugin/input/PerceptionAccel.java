313b7fb43ccd7080bec0b8fecc40029b6642ed77
tc <tc@box>
2015-02-03 18:20:42 +0000

    ++ test commit (if IRCBot is active now)

---
+++ nars_java/nars/plugin/input/PerceptionAccel.java
@@ -0,0 +1,13 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+package nars.plugin.input;
+
+/**
+ *
+ * @author tc
+ */
+public class PerceptionAccel {
+    
+}
