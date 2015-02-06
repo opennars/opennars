03e044345d4579f00bfed5c47d9feaffddeb641e
tc <tc@box>
2015-02-06 21:12:35 +0000

    putting all ideas together to find a way to make this happen guranteedly correctly.

--- nars_java/nars/plugin/input/PerceptionAccel.java
+++ nars_java/nars/plugin/input/PerceptionAccel.java
@@ -13,6 +13,7 @@ import nars.core.control.NAL;
 import nars.entity.Concept;
 import nars.entity.Task;
 import nars.language.Conjunction;
+import nars.language.Interval;
 import nars.language.Term;
 
 /**
@@ -25,15 +26,40 @@ public class PerceptionAccel implements Plugin, EventEmitter.EventObserver {
     public boolean setEnabled(NAR n, boolean enabled) {
         //register listening to new events:
         n.memory.event.set(this, enabled, Events.InduceSucceedingEvent.class);
-
         return true;
     }
     
     ArrayList<Task> eventbuffer=new ArrayList<>();
     int cur_maxlen=10;
     
-    public void perceive() { //implement Peis idea here now
-        
+    public void perceive(NAL nal) { //implement Peis idea here now
+        for(int Len=1;Len<=cur_maxlen;Len++) {
+            //ok, this is the length we have to collect, measured from the end of event buffer
+            Term[] relterms; //=new Term[2*Len-1]; //there is a interval term for every event
+            //measuring its distance to the next event, but for the last event this is obsolete
+            //thus it are 2*Len-1] terms
+            
+            //but it is not that easy, because it can also happen, that tasks happen in parallel,
+            //in which a parallel conjunction has to be formed.
+            //such a parallel conjunction then forms just one term!
+            //so we have to determine how many of them exist at first,
+            //if all of them happen at the same time the entire statement should
+            //become an parallel conjunction..
+            
+            int k=0;
+            for(int i=0;i<Len;i++) {
+                int j=eventbuffer.size()-1-2*(Len-1)+i*2; //we count in 2-sized steps size amount of elements till to the end of the event buffer
+                //
+                Task current=eventbuffer.get(j);
+                relterms[k]=current.sentence.term;
+                if(i!=Len-1) { //if its not the last one, then there is a next one for which we have to put an interval
+                    Task next=eventbuffer.get(j+1);
+                    relterms[k+1]=Interval.interval(next.sentence.getOccurenceTime()-current.sentence.getOccurenceTime(), nal.memory);
+                }
+                
+            }
+            
+        }
     }
     
     //keep track of how many conjunctions with related amount of component terms there are:
@@ -48,7 +74,6 @@ public class PerceptionAccel implements Plugin, EventEmitter.EventObserver {
         } else {
             sv[c.term.length]--;
         }
-        
     }
     
     @Override
@@ -56,11 +81,11 @@ public class PerceptionAccel implements Plugin, EventEmitter.EventObserver {
         if (event == Events.InduceSucceedingEvent.class) { //todo misleading event name, it is for a new incoming event
             Task newEvent = (Task)args[0];
             eventbuffer.add(newEvent);
-            if(eventbuffer.size()>cur_maxlen) {
+            while(eventbuffer.size()>cur_maxlen) {
                 eventbuffer.remove(0);
             }
             NAL nal= (NAL)args[1];
-            perceive();
+            perceive(nal);
         }
         if(event == Events.ConceptForget.class) {
             Concept forgot=(Concept) args[0];
@@ -71,5 +96,4 @@ public class PerceptionAccel implements Plugin, EventEmitter.EventObserver {
             handleConjunctionSequence(newC.term,true);
         }
     }
-    
 }
