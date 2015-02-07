607411527ed754ea8033547aeea3ba2c94711fa1
SeH <1s1e1h1@gmail.com>
2015-02-06 19:39:45 -0500

    working example of invoking clojure core.logic from java

--- nars_lab/src/main/java/nars/clojure/TestCoreLogic.java
+++ nars_lab/src/main/java/nars/clojure/TestCoreLogic.java
@@ -1,8 +1,10 @@
 package nars.clojure;
 
 import clojure.java.api.Clojure;
-import clojure.lang.IFn;
+import clojure.lang.*;
+
 import static java.lang.System.*;
+import static clojure.java.api.Clojure.*;
 
 /**
  * Created by me on 2/6/15.
@@ -14,20 +16,73 @@ public class TestCoreLogic {
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName("clojure.java.api.Clojure");
 
+        IFn require = var("clojure.core", "require");
+        IFn use = var("clojure.core", "use");
+        IFn deref = var("clojure.core", "deref");
+        IFn macroexpand = var("clojure.core", "macroexpand");
+        IFn eval = var("clojure.core", "eval");
+
+        use.invoke(read("clojure.core.logic"));
+
+        /*
         IFn plus = Clojure.var("clojure.core", "+");
         out.println(plus.invoke(1, 2));
-
-        IFn require = Clojure.var("clojure.core", "require");
-        //require.invoke(Clojure.read("clojure.set"));
-        require.invoke(Clojure.read("clojure.logic"));
 
         IFn map = Clojure.var("clojure.core", "map");
         IFn inc = Clojure.var("clojure.core", "inc");
         out.println( map.invoke(inc, Clojure.read("[1 2 3]")) );
 
         IFn printLength = Clojure.var("clojure.core", "*print-length*");
-        IFn deref = Clojure.var("clojure.core", "deref");
         out.println(deref.invoke(printLength));
+        */
+
+        IFn runStar = var("clojure.core.logic", "run*");
+        IFn lrun = var("clojure.core.logic", "run");
+
+
+        LazySeq q = (LazySeq) eval.invoke(macroexpand.invoke(read("(run* [q] (== q 1))")));
+        q.forEach(x -> {
+            System.out.println(x);
+        });
+        System.out.println();
+
+        LazySeq q2 = (LazySeq)eval.invoke(macroexpand.invoke(
+                ArraySeq.create(runStar,
+                        read("[q]"),
+                        read("(== q 1)"))
+        ));
+        q2.forEach(x -> {
+            System.out.println(x);
+        });
+        System.out.println();
+
+        LazySeq q3 = (LazySeq)eval.invoke(macroexpand.invoke(
+                ArraySeq.create(runStar,
+                        read("[q]"),
+                        read("(membero q [1 2 3])"),
+                        read("(membero q [2 3 4])"))
+        ));
+        q3.forEach(x -> {
+            System.out.println(x);
+        });
+//        out.println(
+//                c
+////                        lrun.invoke(
+////                        read("[q]"),
+////                        read("(== q 1)")
+////                )
+//        );
+////
+//        out.println(
+//                lrun.invoke(
+//                        read("1"),
+//                        read("[q]"),
+//                        read("(membero q [1 2 3])"),
+//                        read("(membero q [2 3 4])")
+//                )
+//        );
+
+
 
 
     }
