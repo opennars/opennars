063eedbedc84d6f6df704d41d01b52a6ea1df06e
tc <tc@box>
2015-02-12 00:34:54 +0000

    finally manual tense handling correct..

--- nars_java/nars/inference/RuleTables.java
+++ nars_java/nars/inference/RuleTables.java
@@ -166,19 +166,27 @@ public class RuleTables {
                                     Term C=Conjunction.make(residue,conj.getTemporalOrder());
                                     Implication resImp=Implication.make(C, imp.getPredicate(), imp.getTemporalOrder());
                                     //todo add
-                                    TruthValue truth=TruthFunctions.deduction(s.truth, task.sentence.truth);
                                     Stamp st=new Stamp(task.sentence.stamp,nal.memory.time());
-                                    boolean eternal1=nal.getCurrentBelief().isEternal();
-                                    boolean eternal2=task.sentence.isEternal();
-                                    if(!(eternal1 && eternal2)) { //only eternal if both are eternal
-                                        if(!eternal1) {
-                                            st.setOccurrenceTime(nal.getCurrentBelief().getOccurenceTime());
-                                        } else {
-                                            st.setOccurrenceTime(task.sentence.getOccurenceTime());
-                                        }
-                                    } else { //it should be eternal else
-                                        st.setOccurrenceTime(Stamp.ETERNAL);
+                                    boolean eternalBelieve=nal.getCurrentBelief().isEternal(); //https://groups.google.com/forum/#!searchin/open-nars/projection/open-nars/8KnAbKzjp4E/rBc-6V5pem8J
+                                    boolean eternalTask=task.sentence.isEternal();
+                                    
+                                    TruthValue BelieveTruth=nal.getCurrentBelief().truth;
+                                    TruthValue TaskTruth=task.sentence.truth;
+                                    
+                                    if(eternalBelieve && !eternalTask) { //occurence time of task
+                                        st.setOccurrenceTime(task.sentence.getOccurenceTime());
                                     }
+                                    
+                                    if(!eternalBelieve && eternalTask) { //eternalize case
+                                        BelieveTruth=TruthFunctions.eternalize(BelieveTruth);
+                                    }
+                                    
+                                    if(!eternalBelieve && !eternalTask) { //project believe to task
+                                        BelieveTruth=nal.getCurrentBelief().projectionTruth(task.sentence.getOccurenceTime(), memory.time());
+                                    }
+
+                                    TruthValue truth=TruthFunctions.deduction(BelieveTruth, TaskTruth);
+                                    
                                     st.getChain().add(t);
                                     Sentence S=new Sentence(resImp,s.punctuation,truth,st);
                                     Task Tas=new Task(S,new BudgetValue(BudgetFunctions.forward(truth, nal)));
