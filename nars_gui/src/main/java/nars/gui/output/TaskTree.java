67da46e8f8a61c3ee94561d8746cd413b8644791
SeH <1s1e1h1@gmail.com>
2015-02-16 00:36:38 -0500

    task tree panel updates ..

--- nars_gui/src/main/java/nars/gui/output/TaskTree.java
+++ nars_gui/src/main/java/nars/gui/output/TaskTree.java
@@ -1,6 +1,7 @@
 package nars.gui.output;
 
 import automenta.vivisect.Video;
+import automenta.vivisect.swing.NSlider;
 import nars.core.Events.FrameEnd;
 import nars.core.Events.TaskAdd;
 import nars.core.Events.TaskRemove;
@@ -16,13 +17,11 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.border.MatteBorder;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeCellRenderer;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreeNode;
+import javax.swing.tree.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedDeque;
@@ -30,77 +29,82 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 import static java.awt.BorderLayout.NORTH;
 
 /**
- *
  * @author me
  */
 public class TaskTree extends ReactionPanel implements Reaction, Runnable {
 
-    long updatePeriodMS = 250;
-    DefaultMutableTreeNode root;
-    private DefaultTreeModel model;
-    Map<Task, DefaultMutableTreeNode> nodes = new ConcurrentHashMap();
-    private final JTree tree;
-    final WeakHashMap<Task, TaskLabel> components = new WeakHashMap<>();
-    float priorityThreshold = 0.01f;
+    final WeakHashMap<Task, TaskComponent> components = new WeakHashMap<>();
     final Set<TreeNode> needRefresh = Collections.synchronizedSet(new HashSet());
     final ConcurrentLinkedDeque<Task> toAdd = new ConcurrentLinkedDeque<>();
     final ConcurrentLinkedDeque<Task> toRemove = new ConcurrentLinkedDeque<>();
+    private final JTree tree = new JTree();
+    long updatePeriodMS = 250;
+    DefaultMutableTreeNode root;
+    Map<Task, DefaultMutableTreeNode> nodes = new ConcurrentHashMap();
+    float priorityThreshold = 0.01f;
     long lastUpdateTime = 0;
-    
     boolean needsRestart = false;
-    boolean showingJudgments = false;
+
+    boolean showingJudgments = true;
     boolean showingQuestions = true;
     boolean showingGoals = true;
+
+    private DefaultTreeModel model;
 
     public TaskTree(NAR nar) {
         super(nar, new BorderLayout());
 
-        tree = new JTree();
+        tree.setEditable(true);
+
 
 
         tree.setRootVisible(false);
         tree.setShowsRootHandles(true);
 
 
-        tree.setCellRenderer(new CustomDefaultRenderer());
+        tree.setCellRenderer(cellRenderer);
+        tree.setCellEditor(editor);
 
 
         JPanel menu = new JPanel(new WrapLayout(FlowLayout.LEFT));
-        
+
         JCheckBox showJudgments = new JCheckBox("Judgments");
         showJudgments.setSelected(showingJudgments);
         showJudgments.addActionListener(new ActionListener() {
-            @Override public void actionPerformed(ActionEvent e) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 showingJudgments = showJudgments.isSelected();
                 reset();
-            }            
+            }
         });
         menu.add(showJudgments);
         JCheckBox showQuestions = new JCheckBox("Questions");
         showQuestions.setSelected(showingQuestions);
         showQuestions.addActionListener(new ActionListener() {
-            @Override public void actionPerformed(ActionEvent e) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 showingQuestions = showQuestions.isSelected();
                 reset();
-            }            
+            }
         });
         menu.add(showQuestions);
         JCheckBox showGoals = new JCheckBox("Goals");
         showGoals.setSelected(showingGoals);
         showGoals.addActionListener(new ActionListener() {
-            @Override public void actionPerformed(ActionEvent e) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 showingGoals = showGoals.isSelected();
                 reset();
-            }            
+            }
         });
         menu.add(showGoals);
-        
+
         add(menu, NORTH);
         add(new JScrollPane(tree), BorderLayout.CENTER);
 
         reset();
     }
-    
+
     protected void reset() {
 
         components.clear();
@@ -117,15 +121,14 @@ public class TaskTree extends ReactionPanel implements Reaction, Runnable {
     }
 
 
-
     @Override
     public Class[] getEvents() {
-        return new Class[] { TaskAdd.class, TaskRemove.class, FrameEnd.class /*, TaskRemove.class*/ };
+        return new Class[]{TaskAdd.class, TaskRemove.class, FrameEnd.class /*, TaskRemove.class*/};
     }
 
     public void add(Task t) {
         if (isVisible(t))
-            toAdd.add(t);        
+            toAdd.add(t);
     }
 
     public DefaultMutableTreeNode getNode(final Task t) {
@@ -150,7 +153,7 @@ public class TaskTree extends ReactionPanel implements Reaction, Runnable {
         if ((t.sentence.isGoal()) && (!showingGoals)) return false;
         return t.getPriority() >= priorityThreshold;
     }
-    
+
     public void update() {
         //TODO get existing Tasks at the next frame event by new method: memory.getTasks() which iterates all concepts tasklinks
         if (needsRestart) {
@@ -159,35 +162,36 @@ public class TaskTree extends ReactionPanel implements Reaction, Runnable {
                 if (isVisible(t))
                     add(t);
         }
-        
+
         //remove dead tasks
         for (Task t : nodes.keySet()) {
             if (!isVisible(t))
-                toRemove.add(t);            
+                toRemove.add(t);
         }
 
         long now = System.currentTimeMillis();
         if (now - lastUpdateTime > updatePeriodMS) {
-            SwingUtilities.invokeLater(this);            
-        }        
+            SwingUtilities.invokeLater(this);
+        }
     }
 
-    @Override public void run() {
+    @Override
+    public void run() {
         for (Task t : toRemove) {
             DefaultMutableTreeNode node = nodes.remove(t);
-            if (node!=null) {
+            if (node != null) {
                 TreeNode p = node.getParent();
-                node.removeFromParent();                
+                node.removeFromParent();
                 needRefresh.add(p);
                 needRefresh.remove(node);
             }
             components.remove(t);
             toAdd.remove(t);
         }
-        
+
         for (Task t : toAdd) {
             Task parent = t.getParentTask();
-            if (parent!=null && parent.equals(t)) {
+            if (parent != null && parent.equals(t)) {
                 //System.err.println(t + " has parentTask equal to itself");
                 parent = null;
             }
@@ -201,14 +205,14 @@ public class TaskTree extends ReactionPanel implements Reaction, Runnable {
 
             if (parent == null) {
                 //System.out.println(tnode + " add to root");
-                root.add(tnode);                
+                root.add(tnode);
                 needRefresh.add(root);
             } else {
                 DefaultMutableTreeNode pnode = getNode(parent);
 
                 if (pnode != null) {
                     //System.out.println(tnode + "Adding to: " + pnode);
-                    if (tnode.getParent()!=null) { //pnode.isNodeAncestor(tnode)) {
+                    if (tnode.getParent() != null) { //pnode.isNodeAncestor(tnode)) {
                         tnode.removeFromParent();
                     }
                     pnode.add(tnode);
@@ -223,22 +227,71 @@ public class TaskTree extends ReactionPanel implements Reaction, Runnable {
             }
         }
 
-        for (TaskLabel t : components.values())
+        for (TaskComponent t : components.values())
             t.updateTask();
 
         for (TreeNode t : needRefresh)
             model.reload(t);
-        
-        
-        needRefresh.clear();        
+
+
+        needRefresh.clear();
         toAdd.clear();
         toRemove.clear();
-        
-        repaint();        
+
+        repaint();
         lastUpdateTime = System.currentTimeMillis();
     }
-    
-    public class TaskLabel extends JLabel {
+
+    @Override
+    public void event(Class channel, Object[] arguments) {
+
+        if (channel == TaskAdd.class) {
+            add((Task) arguments[0]);
+        } else if (channel == TaskRemove.class) {
+            toRemove.add((Task) arguments[0]);
+        } else if (channel == FrameEnd.class) {
+            update();
+        }
+    }
+
+    public interface TaskComponent {
+        public void updateTask();
+
+        void setSelected(boolean hasFocus);
+    }
+
+    public class TaskEdit extends JPanel implements TaskComponent {
+        private final Task task;
+        private final TaskLabel label;
+
+        public TaskEdit(Task t) {
+            super(new BorderLayout());
+            this.task = t;
+
+            label = new TaskLabel(task);
+            add(label, BorderLayout.NORTH);
+
+            NSlider n = new NSlider(0.5f, 0, 1f);
+            add(n, BorderLayout.CENTER);
+        }
+
+        @Override
+        public void updateTask() {
+            label.updateTask();
+        }
+
+        @Override
+        public void setSelected(boolean selected) {
+            label.setSelected(selected);
+        }
+    }
+
+//    protected void updateComponent(Task t, JLabel c) {
+//        if (c instanceof TaskLabel)
+//            ((TaskLabel)c).updateTask();
+//    }
+
+    public class TaskLabel extends JLabel implements TaskComponent {
         private final Task task;
 
         public TaskLabel(Task t) {
@@ -247,69 +300,61 @@ public class TaskTree extends ReactionPanel implements Reaction, Runnable {
             //setOpaque(false);
             setOpaque(true);
             setFont(Video.monofont);
-            
+
             updateTask();
         }
 
-        protected void updateTask() {
+        public void updateTask() {
             final Task t = task;
-            
+
             Concept con = nar.memory.concept(t.getTerm());
             float conPri = 0;
             if (con == null) {
                 /*System.err.println("TaskTree: " + t + " missing concept.  either memory was reset or concept should have been created but wasnt.");*/
-                //toRemove.add(t);            
-            }
-            else {
+                //toRemove.add(t);
+            } else {
                 conPri = con.getPriority();
             }
             float taskPri = t.getPriority();
             TruthValue desire = t.getDesire();
-            
+
             Color iColor;
-            if (desire!=null) {
+            if (desire != null) {
                 float confidence = t.getDesire().getConfidence();
-                iColor = new Color(0,confidence/1.5f,conPri/1.5f,0.75f + 0.25f * confidence);
-            }        
-            else {
-                iColor = new Color(0,0,conPri/1.5f,1f);
+                iColor = new Color(0, confidence / 1.5f, conPri / 1.5f, 0.75f + 0.25f * confidence);
+            } else {
+                iColor = new Color(0, 0, conPri / 1.5f, 1f);
             }
-            setBorder(new MatteBorder(0,15,0,0,iColor));
-            
+            setBorder(new MatteBorder(0, 15, 0, 0, iColor));
+
 
             float meanPri = (conPri + taskPri) / 2f;
             final float mpi = 0.5f + 0.5f * meanPri;
             setForeground(new Color(mpi, mpi, mpi));
-            
+
             final float hue = 0.3f + 0.5f * conPri;
             Color c = Color.getHSBColor(hue, 0.4f, conPri * 0.2f);
             //setBackground(new Color(1f-taskPri/4f,1f,1f-taskPri/4f));
             setBackground(c);
-            
+
             setFont(Video.monofont.deriveFont(14f + taskPri * 4f));
             setText(t.toStringExternal2(false));
-            
+
         }
 
         public void setSelected(boolean selected) {
             final int bt = 1;
             if (!selected) {
-                setBorder(new EmptyBorder(bt,bt,bt,bt));
-            }
-            else {
+                setBorder(new EmptyBorder(bt, bt, bt, bt));
+            } else {
                 setBorder(new LineBorder(Color.GRAY, bt));
             }
 
         }
     }
-    
-//    protected void updateComponent(Task t, JLabel c) {
-//        if (c instanceof TaskLabel)
-//            ((TaskLabel)c).updateTask();
-//    }
-    
-    protected class CustomDefaultRenderer
-            extends DefaultTreeCellRenderer {
+
+
+    final DefaultTreeCellRenderer cellRenderer = new DefaultTreeCellRenderer() {
 
         public Component getTreeCellRendererComponent(
                 JTree tree,
@@ -325,43 +370,51 @@ public class TaskTree extends ReactionPanel implements Reaction, Runnable {
                     expanded, leaf, row,
                     hasFocus);*/
 
-            
-            DefaultMutableTreeNode node = (DefaultMutableTreeNode)value;
+
+            DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
             Object v = node.getUserObject();
             if (v instanceof Task) {
-                Task tv = (Task)v;
-                TaskLabel c = components.get(tv);
+                Task tv = (Task) v;
+                TaskComponent c = components.get(tv);
                 if (c == null) {
-                    c = new TaskLabel(tv);
+                    if (tv.isInput())
+                        c = new TaskEdit(tv);
+                    else
+                        c = new TaskLabel(tv);
                     components.put(tv, c);
-                }
-                else {
+                } else {
                     c.updateTask();
                 }
                 c.setSelected(hasFocus);
-                return c;
+                return (JComponent) c;
             }
-            
+
             return super.getTreeCellRendererComponent(
                     tree, value, selected,
                     expanded, leaf, row,
                     hasFocus);
         }
 
-    }
+    };
 
-    @Override
-    public void event(Class channel, Object[] arguments) {
+    final TreeCellEditor editor = new DefaultTreeCellEditor(tree, cellRenderer) {
+        @Override
+        protected boolean canEditImmediately(EventObject event) {
+            if ((event instanceof MouseEvent) &&
+                    SwingUtilities.isLeftMouseButton((MouseEvent) event)) {
+                MouseEvent me = (MouseEvent) event;
 
-        if (channel == TaskAdd.class) {
-            add((Task)arguments[0]);
+
+                return ((me.getClickCount() >= 1) &&
+                        inHitRegion(me.getX(), me.getY()));
+            }
+            return (event == null);
         }
-        else if (channel == TaskRemove.class) {
-            toRemove.add((Task)arguments[0]);
+
+        @Override
+        public Component getTreeCellEditorComponent(JTree tree, Object value, boolean isSelected, boolean expanded, boolean leaf, int row) {
+            return cellRenderer.getTreeCellRendererComponent(tree, value, isSelected, expanded, leaf, row, true);
         }
-        else if (channel == FrameEnd.class) {
-            update();
-        }
-    }
+    };
 
 }
