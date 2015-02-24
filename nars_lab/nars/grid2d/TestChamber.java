f1ecc597d15a82d4bb08b2abb3071d0175080c6e
tc <tc@box>
2015-02-24 06:11:11 +0000

    Parameter change (create own object categories by default)

--- nars_lab/nars/grid2d/TestChamber.java
+++ nars_lab/nars/grid2d/TestChamber.java
@@ -27,7 +27,7 @@ import processing.core.PVector;
 
 public class TestChamber {
 
-    public static boolean staticInformation=true;
+    public static boolean staticInformation=false;
     //TIMING
     static int narUpdatePeriod = 20; /*milliseconds */
     int gridUpdatePeriod = 2;
