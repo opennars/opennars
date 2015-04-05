bb06a7c1832c39cb360dd72f83b2d454a87a5237
tc <tc@box>
2015-04-05 19:23:59 +0000

    improve 2-point regulator example.

--- nars_lab/nars/regulation/twopoint/drawPanel.java
+++ nars_lab/nars/regulation/twopoint/drawPanel.java
@@ -29,16 +29,19 @@ import nars.operator.Operator;
  */
 public class drawPanel extends JPanel {
 
+    int inc=0;
+    int lastinc=0;
     public class move extends Operator {
 
         public move() {
             super("^move");        
         }
         
-        
+
         @Override
         protected List<Task> execute(Operation operation, Term[] args, Memory memory) {
             if(args.length==2) { //left, self
+                inc++;
                 if(args[0].toString().equals("left")) {
                     x-=10;
                     if(x>setpoint) {
@@ -67,7 +70,7 @@ public class drawPanel extends JPanel {
         nar.addPlugin(new move());
         //new NARSwing(nar);
         nar.addInput("<SELF --> [good]>!");
-        new javax.swing.Timer(100, new ActionListener(){
+        new javax.swing.Timer(30, new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
                 repaint();
@@ -75,45 +78,54 @@ public class drawPanel extends JPanel {
         }).start();
     }
             
-    int setpoint=50;
-    int x=10;
+    int setpoint=150;
+    int x=100;
     int y=10;
     int k=0;
     private void doDrawing(Graphics g) {
-        if(k%10==0) {
-        if(x==setpoint) {
-            nar.addInput("<SELF --> [good]>. :|: %1.00;0.90%");
-        }
-        if(k%10==0) {
-            nar.addInput("<SELF --> [good]>!");
-        }
+        int modu=10;
+        boolean cond = (inc!=lastinc);
+        lastinc=inc;
         if(k<1) {
+          //nar.addInput("move(left). :|: %0.00;0.99%");
+         // nar.addInput("move(right). :|: %0.00;0.99%");
+            nar.addInput("move(right)! :|:");
             nar.addInput("move(left)! :|:");
             nar.addInput("move(right)! :|:");
         }
+        if((cond || k%50==0) && x==setpoint) {
+                nar.addInput("<SELF --> [good]>. :|: %1.00;0.90%");
+        }
+        if(cond) {
+            System.out.println(x);
+            
+            if(cond) {
+                nar.addInput("<SELF --> [good]>!");
+            }
+            
 
-        if(x>setpoint) {
-            nar.addInput("<target --> left>. :|:");
-            //nar.addInput("move(left)! :|:");
-        }
-        if(x<setpoint) {
-            nar.addInput("<target --> right>. :|:");
-           // nar.addInput("move(right)! :|:");
-        }
+            if(x>setpoint) {
+                nar.addInput("<target --> left>. :|:");
+                //nar.addInput("move(left)! :|:");
+            }
+            if(x<setpoint) {
+                nar.addInput("<target --> right>. :|:");
+               // nar.addInput("move(right)! :|:");
+            }
         }
         k++;
         
-        System.out.println(x);
+        
         
         nar.step(100);
         Graphics2D g2d = (Graphics2D) g;
 
         g2d.setColor(Color.blue);
-
         g2d.fillOval(x, y, 10, 10);
+        g2d.setColor(Color.red);
         g2d.fillOval(setpoint, y, 10, 10);
         
-        for (int i = 0; i <= 1000; i++) {
+        /*for (int i = 0; i <= 1000; i++) {
 
             Dimension size = getSize();
             Insets insets = getInsets();
@@ -125,7 +137,7 @@ public class drawPanel extends JPanel {
             int x = Math.abs(r.nextInt()) % w;
             int y = Math.abs(r.nextInt()) % h;
             g2d.drawLine(x, y, x, y);
-        }
+        }*/
     }
 
     @Override
