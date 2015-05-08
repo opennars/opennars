07b733ae76597d3f465e3a321275bbbd735738b0
tc <tc@box>
2015-05-08 08:16:30 +0000

    decision threshold 0.5 is fine

--- nars_java/nars/core/build/Default.java
+++ nars_java/nars/core/build/Default.java
@@ -125,7 +125,7 @@ public class Default extends Build implements ConceptBuilder {
 
         param.reliance.set(0.9f);
         
-        param.decisionThreshold.set(0.6);
+        param.decisionThreshold.set(0.5);
     
         //add derivation filters here:
         //param.getDefaultDerivationFilters().add(new BeRational());
