22cd81c573a1d518ad0104042100b23379d0c9cd
SeH <1s1e1h1@gmail.com>
2015-05-28 14:49:09 -0400

    indepVarUsedInvalid merge from 1.6.4

--- nars_logic/src/main/java/nars/nal/Variables.java
+++ nars_logic/src/main/java/nars/nal/Variables.java
@@ -1,11 +1,11 @@
 package nars.nal;
 
 import nars.Global;
-import nars.Memory;
 import nars.Symbols;
 import nars.nal.nal1.Inheritance;
 import nars.nal.nal2.Similarity;
 import nars.nal.nal4.Image;
+import nars.nal.nal5.Junction;
 import nars.nal.term.Compound;
 import nars.nal.term.Term;
 import nars.nal.term.Variable;
@@ -19,13 +19,14 @@ import java.util.Random;
  * TODO rename all *substitute* methods to *unify*
  */
 public class Variables {
-    
+
     public static boolean findSubstitute(final char type, final Term term1, final Term term2, final Map<Term, Term> map1, final Map<Term, Term> map2, final Random random) {
-        return findSubstitute(type, term1, term2, new Map[] { map1, map2 }, random);
+        return findSubstitute(type, term1, term2, new Map[]{map1, map2}, random);
     }
-    
-    /** map is a 2-element array of HashMap<Term,Term>. it may be null, in which case
-     * the maps will be instantiated as necessary.  
+
+    /**
+     * map is a 2-element array of HashMap<Term,Term>. it may be null, in which case
+     * the maps will be instantiated as necessary.
      * this is to delay the instantiation of the 2 HashMap until necessary to avoid
      * wasting them if they are not used.
      */
@@ -33,12 +34,12 @@ public class Variables {
 
         final boolean term1HasVar = term1.hasVar(type);
         final boolean term2HasVar = term2.hasVar(type);
-        
-        
+
+
         final boolean term1Var = term1 instanceof Variable;
         final boolean term2Var = term2 instanceof Variable;
         final boolean termsEqual = term1.equals(term2);
-        if (!term1Var && !term2Var && termsEqual)  {
+        if (!term1Var && !term2Var && termsEqual) {
             return true;
         }
         
@@ -49,19 +50,22 @@ public class Variables {
 
         int ds = 4; //initial size of hashmap below TODO tune
 
-        Term t;                
+        Term t;
         if (term1Var && (((Variable) term1).getType() == type)) {
-            final Variable var1 = (Variable) term1;            
-            t = map[0]!=null ? map[0].get(var1) : null;
-            
+            final Variable var1 = (Variable) term1;
+            t = map[0] != null ? map[0].get(var1) : null;
+
             if (t != null) {
                 return findSubstitute(type, t, term2, map, random);
             } else {
-                
-                if (map[0] == null) {  map[0] = Global.newHashMap(ds); map[1] = Global.newHashMap(ds); }
-                
+
+                if (map[0] == null) {
+                    map[0] = Global.newHashMap(ds);
+                    map[1] = Global.newHashMap(ds);
+                }
+
                 if ((term2 instanceof Variable) && (((Variable) term2).getType() == type)) {
-                    Variable CommonVar = makeCommonVariable(term1, term2);                    
+                    Variable CommonVar = makeCommonVariable(term1, term2);
                     map[0].put(var1, CommonVar);
                     map[1].put(term2, CommonVar);
                 } else {
@@ -84,15 +88,18 @@ public class Variables {
                 return true;
             }
         } else if (term2Var && (((Variable) term2).getType() == type)) {
-            final Variable var2 = (Variable) term2;            
-            t = map[1]!=null ? map[1].get(var2) : null;
-            
+            final Variable var2 = (Variable) term2;
+            t = map[1] != null ? map[1].get(var2) : null;
+
             if (t != null) {
                 return findSubstitute(type, term1, t, map, random);
             } else {
-                
-                if (map[0] == null) {  map[0] = Global.newHashMap(ds); map[1] = Global.newHashMap(ds); }
-                
+
+                if (map[0] == null) {
+                    map[0] = Global.newHashMap(ds);
+                    map[1] = Global.newHashMap(ds);
+                }
+
                 map[1].put(var2, term1);
                 if (var2.isCommon()) {
                     map[0].put(var2, term1);
@@ -122,8 +129,8 @@ public class Variables {
             }
             return true;
         }
-        
-        return termsEqual;        
+
+        return termsEqual;
     }
 
 
@@ -134,58 +141,59 @@ public class Variables {
      * @param n The string name to be checked
      * @return Whether the name contains a variable
      */
-    @Deprecated public static boolean containVar(final CharSequence n) {
+    @Deprecated
+    public static boolean containVar(final CharSequence n) {
         if (n == null) return false;
         final int l = n.length();
         for (int i = 0; i < l; i++) {
-            switch (n.charAt(i)) {                
+            switch (n.charAt(i)) {
                 case Symbols.VAR_INDEPENDENT:
                 case Symbols.VAR_DEPENDENT:
                 case Symbols.VAR_QUERY:
                     return true;
             }
-        }        
+        }
         return false;
     }
-    
+
     public static final boolean containVar(final Term[] t) {
         for (final Term x : t)
             if (x instanceof Variable)
                 return true;
         return false;
     }
-    
+
 
     /**
      * To unify two terms
      *
      * @param type The type of variable that can be substituted
-     * @param t The first and second term as an array, which will have been modified upon returning true
+     * @param t    The first and second term as an array, which will have been modified upon returning true
      * @return Whether the unification is possible.  't' will refer to the unified terms
      */
     public static boolean unify(final char type, final Term[] t, final Random random) {
         return unify(type, t[0], t[1], t, random);
     }
 
- 
+
     /**
      * To unify two terms
      *
-     * @param type The type of variable that can be substituted
+     * @param type      The type of variable that can be substituted
      * @param compound1 The compound containing the first term, possibly modified
      * @param compound2 The compound containing the second term, possibly modified
-     * @param t The first and second term as an array, which will have been modified upon returning true
+     * @param t         The first and second term as an array, which will have been modified upon returning true
      * @return Whether the unification is possible.  't' will refer to the unified terms
      */
     public static boolean unify(final char type, final Term t1, final Term t2, final Term[] compound, final Random random) {
         final Map<Term, Term> map[] = new Map[2]; //begins empty: null,null
-        
+
         final boolean hasSubs = findSubstitute(type, t1, t2, map, random);
         if (hasSubs) {
-            final Term a = applySubstituteAndRenameVariables(((Compound)compound[0]), map[0]);
+            final Term a = applySubstituteAndRenameVariables(((Compound) compound[0]), map[0]);
             if (a == null) return false;
 
-            final Term b = applySubstituteAndRenameVariables(((Compound)compound[1]), map[1]);
+            final Term b = applySubstituteAndRenameVariables(((Compound) compound[1]), map[1]);
             if (b == null) return false;
 
             //only set the values if it will return true, otherwise if it returns false the callee can expect its original values untouched
@@ -196,23 +204,25 @@ public class Variables {
         return false;
     }
 
-    /** appliesSubstitute and renameVariables, resulting in a cloned object, 
-     *  will not change this instance  */
+    /**
+     * appliesSubstitute and renameVariables, resulting in a cloned object,
+     * will not change this instance
+     */
     private static Term applySubstituteAndRenameVariables(final Compound t, final Map<Term, Term> subs) {
         if ((subs == null) || (subs.isEmpty())) {
             //no change needed
             return t;
         }
-        
+
         Term r = t.applySubstitute(subs);
-        
+
         if (r == null) return null;
-        
+
         if (r.equals(t)) return t;
-        
+
         return r;
     }
-    
+
 //    /**
 //     * Check whether a string represent a name of a term that contains a query
 //     * variable
@@ -261,12 +271,25 @@ public class Variables {
 //    public static boolean containVarIndep(final CharSequence n) {
 //        return Texts.containsChar(n, Symbols.VAR_INDEPENDENT);
 //    }    
-    
+
     /**
      * test the invalidity of a term which may be using independent variables
      */
     public static boolean indepVarUsedInvalid(Term T) {
-        if(!(T instanceof Inheritance) && !(T instanceof Similarity)) {
+
+
+        //if its a conjunction/disjunction, this is invalid: (&&,<$1 --> test>,<$1 --> test2>), while this isnt: (&&,<$1 --> test ==> <$1 --> test2>,others)
+        //this means we have to go through the conjunction, and check if the component is a indepVarUsedInvalid instance, if yes, return true
+        //
+        if (T instanceof Junction) {
+            for (Term t : ((Compound) T)) {
+                if (indepVarUsedInvalid(t)) {
+                    return true;
+                }
+            }
+        }
+
+        if (!(T instanceof Inheritance) && !(T instanceof Similarity)) {
             return false;
         }
 
@@ -276,7 +299,7 @@ public class Variables {
     /**
      * Check if two terms can be unified
      *
-     * @param type The type of variable that can be substituted
+     * @param type  The type of variable that can be substituted
      * @param term1 The first term to be unified
      * @param term2 The second term to be unified
      * @return Whether there is a substitution
@@ -284,5 +307,5 @@ public class Variables {
     public static boolean hasSubstitute(final char type, final Term term1, final Term term2, final Random random) {
         return findSubstitute(type, term1, term2, new HashMap<>(), new HashMap<>(), random);
     }
-    
+
 }
