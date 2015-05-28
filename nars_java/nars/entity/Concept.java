7277fd17490dad88503a5fd586fe83fae1960e7f
patrick.hammer <patrick.hammer@XamTine-PC.evon.local>
2015-05-29 01:18:17 +0200

    It should be enough to use the (&/,e1,...,en) of  (&/,e1,...,en) =/> goal (it is deduction from desire perspective). TODO: delete outcommented code.

--- nars_java/nars/entity/Concept.java
+++ nars_java/nars/entity/Concept.java
@@ -371,12 +371,13 @@ public class Concept extends Item<Term> implements Termable {
                         if(plan instanceof Conjunction && ((Conjunction)plan).getTemporalOrder()==TemporalRules.ORDER_FORWARD) {
                             Conjunction Cplan=(Conjunction) plan;
                             int occurrenceOffset=0;
-                            for(Term t : Cplan.term) {
-                                if(t instanceof Interval) {
+                            //for(Term t : Cplan.term) {
+                                Term t=Cplan; //we directly deduct the entire (&/ statement like it should be
+                                /*if(t instanceof Interval) {
                                     Interval I=(Interval)t;
                                     occurrenceOffset += I.getTime(nal.memory.param.duration);
                                     continue;
-                                }
+                                }*/
                                 //Concept Ct = nal.memory.concept(t);
                                 /*if(Ct != null && Ct.desires!=null && Ct.desires.size()>0) {
                                     Task toProcess=Ct.desires.get(0);
@@ -404,7 +405,7 @@ public class Concept extends Item<Term> implements Termable {
                                 //END
 
                                 occurrenceOffset+=nal.memory.param.duration.get();
-                            }
+                            //}
                         }
                     }
                 }
