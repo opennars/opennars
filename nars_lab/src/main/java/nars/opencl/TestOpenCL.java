14ecb2e9e36ab3a4a6dfb78020e080c37de0c407
SeH <1s1e1h1@gmail.com>
2015-05-29 21:51:35 -0400

    working jocl example

--- nars_lab/src/main/java/nars/opencl/TestOpenCL.java
+++ nars_lab/src/main/java/nars/opencl/TestOpenCL.java
@@ -1,57 +1,115 @@
 package nars.opencl;
 
-import com.nativelibs4java.opencl.*;
-import com.nativelibs4java.util.IOUtils;
-import org.bridj.Pointer;
+import com.jogamp.opencl.*;
 
 import java.io.IOException;
-import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.util.Random;
 
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
-import static org.bridj.Pointer.allocateFloats;
+import static java.lang.System.nanoTime;
+import static java.lang.System.out;
+import static com.jogamp.opencl.CLMemory.Mem.*;
+import static java.lang.Math.*;
 
 /**
  * Created by me on 5/29/15.
  */
 public class TestOpenCL {
     public static void main(String[] args) throws IOException {
-        CLContext context = JavaCL.createBestContext();
-        CLQueue queue = context.createDefaultQueue();
-        ByteOrder byteOrder = context.getByteOrder();
 
-        int n = 1024;
-        Pointer<Float>
-                aPtr = allocateFloats(n).order(byteOrder),
-                bPtr = allocateFloats(n).order(byteOrder);
+        // set up (uses default CLPlatform and creates context for all devices)
+        CLContext context = CLContext.create();
+        out.println("created "+context);
 
-        for (int i = 0; i < n; i++) {
-            aPtr.set(i, (float)cos(i));
-            bPtr.set(i, (float)sin(i));
+        // always make sure to release the context under all circumstances
+        // not needed for this particular sample but recommented
+        try{
+
+            // select fastest device
+            CLDevice device = context.getMaxFlopsDevice();
+            out.println("using "+device);
+
+            // create command queue on device.
+            CLCommandQueue queue = device.createCommandQueue();
+
+            int elementCount = 1444477;                                  // Length of arrays to process
+            int localWorkSize = min(device.getMaxWorkGroupSize(), 256);  // Local work size dimensions
+            int globalWorkSize = roundUp(localWorkSize, elementCount);   // rounded up to the nearest multiple of the localWorkSize
+
+            // load sources, create and build program
+            //CLProgram program = context.createProgram(TestOpenCL.class.getResourceAsStream("VectorAdd.cl")).build();
+            CLProgram program = context.createProgram(
+                            //"    // OpenCL Kernel Function for element by element vector addition\n" +
+                            "    kernel void VectorAdd(global const float* a, global const float* b, global float* c, int numElements) {\n" +
+                            "\n" +
+                            "        // get index into global data array\n" +
+                            "        int iGID = get_global_id(0);\n" +
+                            "\n" +
+                            "        // bound check (equivalent to the limit on a 'for' loop for standard/serial C code\n" +
+                            "        if (iGID >= numElements)  {\n" +
+                            "            return;\n" +
+                            "        }\n" +
+                            "\n" +
+                            "        // add the vector elements\n" +
+                            "        c[iGID] = a[iGID] + b[iGID];\n" +
+                            "    }\n"
+            ).build();
+
+            // A, B are input buffers, C is for the result
+            CLBuffer<FloatBuffer> clBufferA = context.createFloatBuffer(globalWorkSize, READ_ONLY);
+            CLBuffer<FloatBuffer> clBufferB = context.createFloatBuffer(globalWorkSize, READ_ONLY);
+            CLBuffer<FloatBuffer> clBufferC = context.createFloatBuffer(globalWorkSize, WRITE_ONLY);
+
+            out.println("used device memory: "
+                    + (clBufferA.getCLSize()+clBufferB.getCLSize()+clBufferC.getCLSize())/1000000 +"MB");
+
+            // fill input buffers with random numbers
+            // (just to have test data; seed is fixed -> results will not change between runs).
+            fillBuffer(clBufferA.getBuffer(), 12345);
+            fillBuffer(clBufferB.getBuffer(), 67890);
+
+            // get a reference to the kernel function with the name 'VectorAdd'
+            // and map the buffers to its input parameters.
+            CLKernel kernel = program.createCLKernel("VectorAdd");
+            kernel.putArgs(clBufferA, clBufferB, clBufferC).putArg(elementCount);
+
+            // asynchronous write of data to GPU device,
+            // followed by blocking read to get the computed results back.
+            long time = nanoTime();
+            queue.putWriteBuffer(clBufferA, false)
+                    .putWriteBuffer(clBufferB, false)
+                    .put1DRangeKernel(kernel, 0, globalWorkSize, localWorkSize)
+                    .putReadBuffer(clBufferC, true);
+            time = nanoTime() - time;
+
+            // print first few elements of the resulting buffer to the console.
+            out.println("a+b=c results snapshot: ");
+            for(int i = 0; i < 10; i++)
+                out.print(clBufferC.getBuffer().get() + ", ");
+            out.println("...; " + clBufferC.getBuffer().remaining() + " more");
+
+            out.println("computation took: "+(time/1000000)+"ms");
+
+        }finally{
+            // cleanup all resources associated with this context.
+            context.release();
         }
 
-        // Create OpenCL input buffers (using the native memory pointers aPtr and bPtr) :
-        CLBuffer<Float>
-                a = context.createBuffer(CLMem.Usage.Input, aPtr),
-                b = context.createBuffer(CLMem.Usage.Input, bPtr);
+    }
 
-        // Create an OpenCL output buffer :
-        CLBuffer<Float> out = context.createFloatBuffer(CLMem.Usage.Output, n);
+    private static void fillBuffer(FloatBuffer buffer, int seed) {
+        Random rnd = new Random(seed);
+        while(buffer.remaining() != 0)
+            buffer.put(rnd.nextFloat()*100);
+        buffer.rewind();
+    }
 
-        // Read the program sources and compile them :
-        String src = IOUtils.readText(TestOpenCL.class.getResource("add_floats.cl"));
-        CLProgram program = context.createProgram(src);
-
-        // Get and call the kernel :
-        CLKernel addFloatsKernel = program.createKernel("add_floats");
-        addFloatsKernel.setArgs(a, b, out, n);
-        CLEvent addEvt = addFloatsKernel.enqueueNDRange(queue, new int[] { n });
-
-        Pointer<Float> outPtr = out.read(queue, addEvt); // blocks until add_floats finished
-
-        // Print the first 10 output values :
-        for (int i = 0; i < 10 && i < n; i++)
-            System.out.println("out[" + i + "] = " + outPtr.get(i));
-
+    private static int roundUp(int groupSize, int globalSize) {
+        int r = globalSize % groupSize;
+        if (r == 0) {
+            return globalSize;
+        } else {
+            return globalSize + groupSize - r;
+        }
     }
 }
