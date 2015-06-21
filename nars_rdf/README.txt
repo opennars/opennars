54ffea549ae5bf02ac841295bfeee61dc5755a7c
SeH <1s1e1h1@gmail.com>
2015-06-20 21:00:05 -0400

    added an optimized fork of apache jena into nars_rdf

---
+++ nars_rdf/README.txt
@@ -0,0 +1,12 @@
+Jena2 README
+============
+
+Welcome to Apache Jena,  a Java framework for 
+writing Semantic Web applications.
+
+Documentation can be found on the web at
+http://jena.apache.org/
+
+There is a mailing lists for questions and feedback:
+users@jena.apache.org
+
---
+++ nars_rdf/src/main/java/jena/version.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jena;
+
+import static org.apache.jena.atlas.logging.LogCtl.setCmdLogging;
+
+import java.lang.reflect.*;
+
+import org.apache.jena.Jena ;
+
+/**
+ * jena.version
+ * Print out jena version information and exit.
+ */
+public class version implements Jena {
+
+    static { setCmdLogging(); }
+
+    /**
+	 * Print out jena version information and exit.
+	 * @param args
+	 * @throws IllegalAccessException 
+	 * @throws IllegalArgumentException 
+	 */
+	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
+		Field f[] = Jena.class.getDeclaredFields();
+        for ( Field aF : f )
+        {
+            System.out.println( aF.getName() + ": " + aF.get( null ) );
+        }
+	}
+
+}
