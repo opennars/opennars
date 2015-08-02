3b21a672f5f5e76e0ff54f3a7a2e5deed1fba416
patrick.hammer <patham9@gmail.com>
2015-08-03 00:58:21 +0200

    TODO: Structural rules :)

--- nars_logic/src/main/java/nars/nar/NewDefault.java
+++ nars_logic/src/main/java/nars/nar/NewDefault.java
@@ -1,7 +1,12 @@
 package nars.nar;
 
 import nars.nal.LogicPolicy;
+import nars.nal.LogicRule;
 import nars.nal.NALExecuter;
+import nars.process.concept.*;
+import nars.task.filter.DerivationFilter;
+import nars.task.filter.FilterBelowConfidence;
+import nars.task.filter.FilterDuplicateExistingBelief;
 
 /**
  * Temporary class which uses the new rule engine for ruletables
@@ -10,6 +15,24 @@ public class NewDefault extends Default {
 
     @Override
     public LogicPolicy getLogicPolicy() {
-        return newPolicy(NALExecuter.defaults);
+        return nalex(NALExecuter.defaults);
+    }
+
+    public static LogicPolicy nalex(ConceptFireTaskTerm ruletable) {
+
+        return new LogicPolicy(
+
+                new LogicRule /* <ConceptProcess> */ [] {
+                        new FilterEqualSubtermsInRespectToImageAndProduct(),
+                        ruletable
+                        //---------------------------------------------
+                } ,
+
+                new DerivationFilter[] {
+                        new FilterBelowConfidence(),
+                        new FilterDuplicateExistingBelief(),
+                }
+
+        );
     }
 }
