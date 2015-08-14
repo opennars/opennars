0ee263c7c6fe81ac7f385d4ed3eccd8a5e89ffa4
patrick.hammer <patham9@gmail.com>
2015-08-14 17:25:57 +0200

    Oops forgot STMEventInference (Let's STMInduction of default unchanged, and I will probably use this one to establish the temporal termlinks in the long run, for now its fine that it uses the old principle since it worked)

---
+++ nars_logic/src/main/java/nars/op/mental/STMEventInference.java
@@ -0,0 +1,135 @@
+package nars.op.app;
+
+import nars.AbstractMemory;
+import nars.Events;
+import nars.Global;
+import nars.NAR;
+import nars.event.NARReaction;
+import nars.nal.NALExecuter;
+import nars.nal.nal7.TemporalRules;
+import nars.process.TaskProcess;
+import nars.task.Task;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+import static nars.nal.nal7.TemporalRules.containsMentalOperator;
+import static nars.term.Terms.equalSubTermsInRespectToImageAndProduct;
+
+/**
+ * Short-term memory Event Induction.  Empties task buffer when plugin is (re)started.
+ */
+public class STMEventInference extends NARReaction {
+
+    public final Deque<Task> stm;
+    int stmSize;
+    //public static STMEventInference I=null;
+
+    public STMEventInference(NAR nar) {
+        super(nar);
+        this.stmSize = 1;
+        stm = Global.THREADS == 1 ? new ArrayDeque() : new ConcurrentLinkedDeque<>();
+        //I=this; //hm there needs to be a way to query plugins from the NAR/NAL object like in 1.6.x, TODO find out
+    }
+
+    @Override
+    public Class[] getEvents() {
+        return new Class[]{TaskProcess.class, Events.ResetStart.class};
+    }
+
+
+    @Override
+    public void event(Class event, Object[] args) {
+        if (event == TaskProcess.class) {
+            Task t = (Task) args[0];
+            TaskProcess n = (TaskProcess) args[1];
+            inductionOnSucceedingEvents(t, n, false);
+        }
+        else if (event == Events.ResetStart.class) {
+            stm.clear();
+        }
+    }
+
+    public static boolean isInputOrTriggeredOperation(final Task newEvent, AbstractMemory mem) {
+        if (newEvent.isInput()) return true;
+        if (containsMentalOperator(newEvent)) return true;
+        return newEvent.getCause() != null;
+    }
+
+    public int getStmSize() {
+        return stmSize;
+    }
+
+    public boolean inductionOnSucceedingEvents(final Task currentTask, TaskProcess nal, boolean anticipation) {
+
+        stmSize = nal.memory.param.shortTermMemoryHistory.get();
+
+        if (currentTask == null || (!currentTask.isTemporalInductable() && !anticipation)) { //todo refine, add directbool in task
+            return false;
+        }
+
+
+        if (currentTask.sentence.isEternal() || (!isInputOrTriggeredOperation(currentTask, nal.memory) && !anticipation)) {
+            return false;
+        }
+
+        //new one happened and duration is already over, so add as negative task
+        nal.emit(Events.EventBasedReasoningEvent.class, currentTask, nal);
+
+        //final long now = nal.memory.time();
+
+
+        Iterator<Task> ss = stm.iterator();
+
+        int numToRemoveFromBeginning = stm.size() - stmSize;
+
+        while (ss.hasNext()) {
+
+            Task stmLast = ss.next();
+
+
+            if (!equalSubTermsInRespectToImageAndProduct(currentTask.sentence.getTerm(), stmLast.sentence.getTerm())) {
+                continue;
+            }
+
+
+            if (numToRemoveFromBeginning > 0) {
+                ss.remove();
+            }
+        }
+
+
+        //iterate on a copy because temporalInduction seems like it sometimes calls itself recursively and this will cause a concurrent modification exception otherwise
+        Task[] stmCopy = stm.toArray(new Task[stm.size()]);
+
+        for (Task previousTask : stmCopy) {
+
+
+
+
+            //nal.setCurrentTask(currentTask);
+
+            //nal.setBelief(previousTask);
+
+            //if(currentTask.getPriority()>Parameters.TEMPORAL_INDUCTION_MIN_PRIORITY)
+            //TemporalRules.temporalInduction(currentTask, previousTask,
+                    //nal.newStamp(currentTask.sentence, previousTask.sentence),
+            //        nal);
+            NALExecuter.THIS.reason(currentTask, previousTask.getSentence(), previousTask.getTerm(), nal);
+        }
+
+        ////for this heuristic, only use input events & task effects of operations
+        ////if(currentTask.getPriority()>Parameters.TEMPORAL_INDUCTION_MIN_PRIORITY) {
+        //stmLast = currentTask;
+        ////}
+        while (stm.size() > stmSize) {
+            stm.removeFirst();
+        }
+        stm.add(currentTask);
+
+        return true;
+    }
+
+}
