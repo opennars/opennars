b56ccf9b9dbfa179f095a95671b61f8a3c5edbfb
PtrMan <robertw89@googlemail.com>
2015-08-15 00:03:07 +0200

    testbed: implemented reflection for Component-ScriptAccessor, old style methods are still there TODO: remove

--- nars_lab/src/main/java/ptrman/difficultyEnvironment/scriptAccessors/ComponentManipulationScriptingAccessor.java
+++ nars_lab/src/main/java/ptrman/difficultyEnvironment/scriptAccessors/ComponentManipulationScriptingAccessor.java
@@ -4,13 +4,66 @@ import ptrman.difficultyEnvironment.animation.AnimationEvent;
 import ptrman.difficultyEnvironment.animation.ExecuteJavascriptAnimationEvent;
 import ptrman.difficultyEnvironment.interactionComponents.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-// TODO< use reflection to abstract this code (just call the matching constructor) ? >
 /**
  *
  */
 public class ComponentManipulationScriptingAccessor {
+    private Set<Class<? extends IComponent>> allComponentClasses = new HashSet<>();
+
+    public ComponentManipulationScriptingAccessor() {
+        allComponentClasses.add(AnimationComponent.class);
+        allComponentClasses.add(BiasedRandomAIComponent.class);
+        allComponentClasses.add(JavascriptComponent.class);
+        allComponentClasses.add(TopDownViewWheeledPhysicsComponent.class);
+        allComponentClasses.add(TopDownViewWheeledControllerComponent.class);
+        allComponentClasses.add(TwoDimensionalRaysComponent.class);
+    }
+
+    public IComponent createByComponentClassName(String className, Object... constructorParameters) {
+        try {
+            for( Class<? extends IComponent> iterationClass : allComponentClasses ) {
+                if( iterationClass.getCanonicalName().equals(className) ) {
+                    if( constructorParameters.length == 0 ) {
+                        return iterationClass.getConstructor().newInstance();
+                    }
+                    else if( constructorParameters.length == 1 ) {
+                        return iterationClass.getConstructor(constructorParameters[0].getClass()).newInstance(constructorParameters[0]);
+                    }
+                    else if( constructorParameters.length == 2 ) {
+                        return iterationClass.getConstructor(constructorParameters[0].getClass(), constructorParameters[1].getClass()).newInstance(constructorParameters[0],constructorParameters[1]);
+                    }
+                    else if( constructorParameters.length == 3 ) {
+                        return iterationClass.getConstructor(constructorParameters[0].getClass(), constructorParameters[1].getClass(), constructorParameters[2].getClass()).newInstance(constructorParameters[0],constructorParameters[1],constructorParameters[2]);
+                    }
+                    else if( constructorParameters.length == 4 ) {
+                        return iterationClass.getConstructor(constructorParameters[0].getClass(), constructorParameters[1].getClass(), constructorParameters[2].getClass(), constructorParameters[3].getClass()).newInstance(constructorParameters[0],constructorParameters[1],constructorParameters[2],constructorParameters[3]);
+                    }
+                    else if( constructorParameters.length == 5 ) {
+                        return iterationClass.getConstructor(constructorParameters[0].getClass(), constructorParameters[1].getClass(), constructorParameters[2].getClass(), constructorParameters[3].getClass(), constructorParameters[4].getClass()).newInstance(constructorParameters[0],constructorParameters[1],constructorParameters[2],constructorParameters[3], constructorParameters[4]);
+                    }
+                    else {
+                        throw new InternalError();
+                    }
+                }
+            }
+        } catch (InstantiationException e) {
+            throw new InternalError();
+        } catch (IllegalAccessException e) {
+            throw new InternalError();
+        } catch (InvocationTargetException e) {
+            throw new InternalError();
+        } catch (NoSuchMethodException e) {
+            throw new InternalError();
+        }
+
+        throw new InternalError();
+    }
+
     public IComponent createAnimationComponent(List animationEvents) {
         AnimationComponent result = new AnimationComponent();
 
