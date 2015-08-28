a7a6295c6a65355e31c193fdb1f99b5cdbee9b28
patrick.hammer <patham9@gmail.com>
2015-08-29 01:02:41 +0200

    Efficient implementation of contains recusrively (to losen the work for seh a bit, my favorite solution here from elegancy perspective would be worse in performance ^^)

--- nars_logic/src/main/java/nars/meta/pre/NoCommonSubterm.java
+++ nars_logic/src/main/java/nars/meta/pre/NoCommonSubterm.java
@@ -5,6 +5,9 @@ import nars.meta.pre.PreCondition2;
 import nars.term.Compound;
 import nars.term.Term;
 import nars.term.Terms;
+import nars.term.Variable;
+
+import java.util.HashSet;
 
 /**
  * Created by me on 8/15/15.
@@ -14,18 +17,36 @@ public class NoCommonSubterm extends PreCondition2 {
         super(arg1, arg2);
     }
 
+    public boolean Add_All_Subterms_Recursively_As_Long_As_Not_In_Z(Term t, HashSet<Term> map, HashSet<Term> Z) {
+        if(Z.contains(t)) { //by this we can stop early
+            return false;
+        }
+        map.add(t);
+        if(t instanceof Compound) {
+            Compound c = (Compound) t;
+            for(Term d : c) {
+                boolean ret = Add_All_Subterms_Recursively_As_Long_As_Not_In_Z(d, map, Z);
+                if(!ret) { //by this we can stop early
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean Share_Any_Subterm(Term a, Term b) {
+        HashSet<Term> A = new HashSet<>();
+        HashSet<Term> B = new HashSet<>();
+        Add_All_Subterms_Recursively_As_Long_As_Not_In_Z(a, A, new HashSet<Term>());
+        if(!Add_All_Subterms_Recursively_As_Long_As_Not_In_Z(b, B, A)) //we stop early this way (efficiency)
+            return true;
+        return false;
+    }
+
     @Override
     public boolean test(RuleMatch m, Term a, Term b) {
-        //TODO this will only compare the first level of subterms
-        //for recursive, we will need a stronger test
-        //but we should decide if recursive is actually necessary
-        //and create alternate noCommonSubterm and noCommonRecursiveSubterm
-        //preconditions to be entirely clear
-
-        if ((a instanceof Compound) && (b instanceof Compound))
-            if (Terms.shareAnySubTerms((Compound) a, (Compound) b))
-                return false;
-
+        if(Share_Any_Subterm(a,b))
+            return false;
         return true;
     }
 
