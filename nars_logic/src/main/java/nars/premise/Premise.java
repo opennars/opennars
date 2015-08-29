efc0da80306287562b6550e30ba52225dfd23948
patrick.hammer <patham9@gmail.com>
2015-08-29 02:02:39 +0200

    Premise is a mess, its not clear what this code here should even do. I am unable to fix the breakage of premise selection sorry.

--- nars_logic/src/main/java/nars/premise/Premise.java
+++ nars_logic/src/main/java/nars/premise/Premise.java
@@ -289,10 +289,10 @@ public interface Premise {
         final Task currentBelief = getBelief();
         if (currentTask.isJudgment() || currentBelief == null) {
             ptask = currentTask;
-        } /*else { nope no cheats. NAL5 is not for now and this cheating is not specified.
+        } else { //Unspecified cheat we need to get rid of.
             // to answer a question with negation in NAL-5 --- move to activated task?
             ptask = currentBelief;
-        }*/
+        }
 
 
         return deriveSingle(newTask(newContent, punctuation)
