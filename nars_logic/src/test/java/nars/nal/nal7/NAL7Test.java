f6335430df3d4932be30831766878ef9d5510acd
patrick.hammer <patham9@gmail.com>
2015-10-14 17:04:02 +0200

    Going on with NAL7 Testcases.

--- nars_logic/src/test/java/nars/nal/nal7/NAL7Test.java
+++ nars_logic/src/test/java/nars/nal/nal7/NAL7Test.java
@@ -70,11 +70,84 @@ public class NAL7Test extends AbstractNALTest {
         TestNAR tester = test();
         tester.nar.stdout();
 
-        tester.believe("<(&/,<($x, key_101) --> hold>,/5) =/> <($x, room_101) --> enter>>", 0.95f, 0.9f);
+        tester.believe("<(&/,<($x, key_101) --> hold>,/5) =/> <($x, room_101) --> enter>>", 1.0f, 0.9f);
         tester.believe("<(John, key_101) --> hold>. :\\:");
 
-        tester.mustBelieve(cycles, "<(*,John,room_101) --> enter>", 0.95f, 0.81f); //":\:" TODO HOW TEST FOR OCCURENCE?
+        tester.mustBelieve(cycles, "<(*,John,room_101) --> enter>", 1.00f, 0.81f); //":\:" TODO HOW TEST FOR OCCURENCE? (really OCCURENCE, not creation!!)
         tester.run();
     }
+
+    @Test
+    public void inference_on_tense_2() throws InvalidInputException {
+        TestNAR tester = test();
+        tester.nar.stdout();
+
+        tester.believe("<(&/,<($x, key_101) --> hold>,/5) =/> <($x, room_101) --> enter>>", 1.0f, 0.9f);
+        tester.believe("<(*,John,room_101) --> enter>. :|:");
+
+        tester.mustBelieve(cycles, "<(John, key_101) --> hold>", 1.00f, 0.45f); //":\:" TODO HOW TEST FOR OCCURENCE? (really OCCURENCE, not creation!!)
+        tester.run();
+    }
+
+    @Test
+    public void inference_on_tense_3() throws InvalidInputException {
+        TestNAR tester = test();
+        tester.nar.stdout();
+
+        tester.believe("<<(*,John,key_101) --> hold> =/> <(*,John,room_101) --> enter>>", 1.0f, 0.9f);
+        tester.believe("<(*,John,key_101) --> hold>. :|:");
+
+        tester.mustBelieve(cycles, "<(*,John,room_101) --> enter>", 1.00f, 0.81f); //":/:" TODO HOW TEST FOR OCCURENCE? (really OCCURENCE, not creation!!)
+        tester.run();
+    }
+
+    @Test
+    public void inference_on_tense_4() throws InvalidInputException {
+        TestNAR tester = test();
+        tester.nar.stdout();
+
+        tester.believe("<<(*,John,key_101) --> hold> =/> <(*,John,room_101) --> enter>>", 1.0f, 0.9f);
+        tester.believe("<(*,John,room_101) --> enter>. :|:");
+
+        tester.mustBelieve(cycles, "<(*,John,key_101) --> hold>", 1.00f, 0.45f); //:\\: TODO HOW TEST FOR OCCURENCE? (really OCCURENCE, not creation!!)
+        tester.run();
+    }
+
+    @Test
+    public void induction_on_events() throws InvalidInputException {
+        TestNAR tester = test();
+        tester.nar.stdout();
+
+        tester.believe("<(*,John,door_101) --> open>. :|:");
+        tester.believe("<(*,John,room_101) --> enter>. :|:");
+
+        tester.mustBelieve(cycles, "<<(*,John,room_101) --> enter> =\\> (&/,<(*,John,door_101) --> open>)>", 1.00f, 0.45f); // :|: TODO HOW TEST FOR OCCURENCE?
+        tester.run();
+    }
+
+    @Test
+    public void induction_on_events2() throws InvalidInputException {
+        TestNAR tester = test();
+        tester.nar.stdout();
+
+        tester.believe("<(*,John,door_101) --> open>. :|:");
+        tester.believe("<(*,John,room_101) --> enter>. :|:");
+
+        tester.mustBelieve(cycles, "<(&/,<(*,John,door_101) --> open>) =/> <(*,John,room_101) --> enter>>", 1.00f, 0.45f); // :|: TODO HOW TEST FOR OCCURENCE?
+        tester.run();
+    }
+
+    @Test
+    public void induction_on_events3() throws InvalidInputException {
+        TestNAR tester = test();
+        tester.nar.stdout();
+
+        tester.believe("<(*,John,door_101) --> open>. :|:");
+        tester.believe("<(*,John,room_101) --> enter>. :|:");
+
+        tester.mustBelieve(cycles, "<(&/,<(*,John,door_101) --> open>) </> <(*,John,room_101) --> enter>>", 1.00f, 0.45f); // :|: TODO HOW TEST FOR OCCURENCE?
+        tester.run();
+    }
+
 
 }
