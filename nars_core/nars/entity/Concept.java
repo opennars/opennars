b542ea4ada80d913a69cf65c54955d4deb5659d4
patham9@gmail.com <patham9@gmail.com>
2016-12-17 01:07:07 +0100

    Wtf where did this import come from. ^^

--- nars_core/nars/entity/Concept.java
+++ nars_core/nars/entity/Concept.java
@@ -25,7 +25,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import com.sun.istack.internal.Pool;
 import nars.inference.TruthFunctions;
 import nars.util.Events.BeliefSelect;
 import nars.util.Events.ConceptBeliefAdd;
