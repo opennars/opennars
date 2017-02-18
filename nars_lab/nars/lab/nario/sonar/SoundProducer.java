8d02f9add68ea5efb887af8af769d4a9053da3ae
patham9@gmail.com <patham9@gmail.com>
2017-02-18 09:43:40 +0100

    Everything in shape now. And Nario is gone.

--- nars_lab/nars/lab/nario/sonar/SoundProducer.java
+++
@@ -1,8 +0,0 @@
-package nars.lab.nario.sonar;
-
-public interface SoundProducer
-{
-    public float read(float[] buf, int readRate);
-    public void skip(int samplesToSkip, int readRate);
-    public boolean isLive();
-}
--- nars_lab/nars/lab/nario/sonar/StereoSoundProducer.java
+++
@@ -1,7 +0,0 @@
-package nars.lab.nario.sonar;
-
-public interface StereoSoundProducer
-{
-    public float read(float[] leftBuf, float[] rightBuf, int readRate);
-    public void skip(int samplesToSkip, int readRate);
-}
