0c94259918c8f77345da07fbfb483e072035b5f3
Patrick Hammer <patham9@gmail.com>
2017-10-02 16:05:46 -0400

    Spatial conjunction supports grouping flattening etc.

--- nars_core/nars/language/Variables.java
+++ nars_core/nars/language/Variables.java
@@ -278,55 +278,11 @@ public class Variables {
         
         return r;
     }
-    
-//    /**
-//     * Check whether a string represent a name of a term that contains a query
-//     * variable
-//     *
-//     * @param n The string name to be checked
-//     * @return Whether the name contains a query variable
-//     */
-//    public static boolean containVarQuery(final CharSequence n) {
-//        return Texts.containsChar(n, Symbols.VAR_QUERY);
-//    }
-
-//    /**
-//     * Check whether a string represent a name of a term that contains a
-//     * dependent variable
-//     *
-//     * @param n The string name to be checked
-//     * @return Whether the name contains a dependent variable
-//     */
-//    public static boolean containVarDep(final CharSequence n) {
-//        return Texts.containsChar(n, Symbols.VAR_DEPENDENT);
-//    }
 
     public static Variable makeCommonVariable(final Term v1, final Term v2) {
         //TODO use more efficient string construction
         return new Variable(v1.toString() + v2.toString() + '$');
     }
-
-//    public static boolean containVarDepOrIndep(final CharSequence n) {
-//        final int l = n.length();
-//        for (int i = 0; i < l; i++) {
-//            char c = n.charAt(i);
-//            if ((c == Symbols.VAR_INDEPENDENT) || (c == Symbols.VAR_DEPENDENT)) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-
-//    /**
-//     * Check whether a string represent a name of a term that contains an
-//     * independent variable
-//     *
-//     * @param n The string name to be checked
-//     * @return Whether the name contains an independent variable
-//     */
-//    public static boolean containVarIndep(final CharSequence n) {
-//        return Texts.containsChar(n, Symbols.VAR_INDEPENDENT);
-//    }    
     
     /**
      * Check whether a term is using an
