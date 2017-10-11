53b7dd1e7c815a38418bfa91a5d5b04680e898c6
Patrick Hammer <patham9@gmail.com>
2017-10-11 15:40:18 -0400

    StructuralRule fix for Robert's example: https://gist.github.com/anonymous/09f4410b6678120c733c91ad26c158c7

--- nars_core/nars/inference/StructuralRules.java
+++ nars_core/nars/inference/StructuralRules.java
@@ -765,15 +765,17 @@ public final class StructuralRules {
     static boolean structuralCompound(CompoundTerm compound, Term component, boolean compoundTask, int index, DerivationContext nal) {
         
         if(compound instanceof Conjunction) {
-            Conjunction conj = (Conjunction) compound; //only for # for now, will be gradually applied to &/ later
-            if(conj.getTemporalOrder() == TemporalRules.ORDER_FORWARD && conj.isSpatial) { //and some also to && &|
-                //flattenSequence(compound, component, compoundTask, index, nal);
-                groupSequence(compound, component, compoundTask, index, nal);
-                //takeOutFromConjunction(compound, component, compoundTask, index, nal);
-                splitConjunctionApart(compound, component, compoundTask, index, nal);
-            }
-            if(conj.getTemporalOrder() == TemporalRules.ORDER_FORWARD) {
-                seqToImage(conj, index, nal);
+            if(nal.getCurrentTask().getTerm() == compound) {
+                Conjunction conj = (Conjunction) compound; //only for # for now, will be gradually applied to &/ later
+                if(conj.getTemporalOrder() == TemporalRules.ORDER_FORWARD && conj.isSpatial) { //and some also to && &|
+                    //flattenSequence(compound, component, compoundTask, index, nal);
+                    groupSequence(compound, component, compoundTask, index, nal);
+                    //takeOutFromConjunction(compound, component, compoundTask, index, nal);
+                    splitConjunctionApart(compound, component, compoundTask, index, nal);
+                }
+                if(conj.getTemporalOrder() == TemporalRules.ORDER_FORWARD) {
+                    seqToImage(conj, index, nal);
+                }
             }
         }
         
