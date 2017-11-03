52204193aa0a3a0ea56714d3e7609b800fe92649
Patrick Hammer <patham9@gmail.com>
2017-11-02 21:15:35 -0400

    Revert, this one was just a test, I didn't want to push it.

--- nars_core/nars/inference/TruthFunctions.java
+++ nars_core/nars/inference/TruthFunctions.java
@@ -400,7 +400,7 @@ public final class TruthFunctions extends UtilityFunctions {
     }
     
     public static final float temporalProjection(final long sourceTime, final long targetTime, final long currentTime) {
-        double a = 10000.0 * Parameters.projectionDecay.get(); //projection less strict as we changed in v2.0.0  10000.0 slower decay than 100000.0
+        double a = 100000.0 * Parameters.projectionDecay.get(); //projection less strict as we changed in v2.0.0  10000.0 slower decay than 100000.0
         return 1.0f - abs(sourceTime - targetTime) / (float) (abs(sourceTime - currentTime) + abs(targetTime - currentTime) + a);
     }
 }
