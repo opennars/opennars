c086c9892fad2e89cdda33004af64f576c1752fc
Patrick Hammer <patham9@gmail.com>
2018-02-02 16:11:19 -0500

    Imagination spaces. This will need a long explanation. Towards learning conditional eye movements to identify faces, and imaging chairs from different angles. Each term has an imagination space (each having a finite memory amount) assigned that can be manipulated (declaratively) to another imagination space with an operator that operates on it. Here, in a program (&/,im1,op1(),im2,op2(),im3) the imagination space of the entire sequence (summarizing the entire space) is not the same as the imagination space of im3 which isn't a summary (such as "face") about it but simply the last state (eye at last position, observing that part of the image)

---
+++ nars_core/nars/imagination/ImaginationSpace.java
@@ -0,0 +1,20 @@
+package nars.imagination;
+
+import nars.entity.TruthValue;
+import nars.language.Conjunction;
+import nars.operator.Operator;
+
+/**
+ *
+ * @author Patrick
+ */
+public interface ImaginationSpace {
+    //a group of operations that when executed add up to a certain declarative "picture".
+    public TruthValue DetermineSimilarityTo(final ImaginationSpace obj);
+    //attaches an imagination space to the conjunction that is constructed
+    //by starting with the leftmost element of the conjunction
+    //and then gradually moving to the right
+    public ImaginationSpace ConstructSpace(Conjunction program);
+    //Has to return a new instance, not changing "this"!
+    public ImaginationSpace ProgressSpace(Operator op, ImaginationSpace B);
+}
