e865bd48dc372ff937ed406d478ff88ce1fbdb08
Patrick Hammer <patham9@gmail.com>
2018-03-29 17:15:15 -0400

    priority modulated by satisfaction.

--- nars_core/nars/control/ConceptProcessing.java
+++ nars_core/nars/control/ConceptProcessing.java
@@ -274,6 +274,7 @@ public class ConceptProcessing {
             }
 
             double Satisfaction=1.0-AntiSatisfaction;
+            task.setPriority(task.getPriority()* (float)AntiSatisfaction);
             TruthValue T=goal.truth.clone();
 
             T.setFrequency((float) (T.getFrequency()-Satisfaction)); //decrease frequency according to satisfaction value
