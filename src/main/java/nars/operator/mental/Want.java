7be7080678c0116ee5bee3e18409f31000ee662b
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2018-05-02 21:53:55 -0400

    docs: added GPL header to all source files.
    ISSUES CLOSED: #93

--- src/main/java/nars/operator/mental/Want.java
+++ src/main/java/nars/operator/mental/Want.java
@@ -1,6 +1,4 @@
-/*
- * Copyright (C) 2014 peiwang
- *
+/**
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -14,7 +12,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package nars.operator.mental;
 
 import com.google.common.collect.Lists;
