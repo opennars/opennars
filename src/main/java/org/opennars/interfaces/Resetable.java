ea4f10091a3ede381625daf49819fff3794060a4
PtrMan <robertw89@googlemail.com>
2018-05-07 15:50:53 +0200

    Update: introduced a lot of interfaces necessary for Reasoner

---
+++ src/main/java/org/opennars/interfaces/Resetable.java
@@ -0,0 +1,22 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.opennars.interfaces;
+
+/**
+ * Implementations can be reseted - that is to flush all content and restore the state to some default state
+ */
+public interface Resetable {
+    void reset();
+}
---
+++ src/main/java/org/opennars/interfaces/StringNarsese.java
@@ -0,0 +1,25 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.opennars.interfaces;
+
+/**
+ * Something which can work with Narsese as a string representation
+ */
+public interface StringNarsese {
+    // TODO< split this and refactor to interface which can be used by the parser too >
+    void addInput(String text);
+
+    void addInputFile(final String s);
+}
