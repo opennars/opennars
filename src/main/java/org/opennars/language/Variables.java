4e195c9911411488681bc80971db4139cb3cd976
PtrMan <robertw89@googlemail.com>
2018-05-09 19:48:01 +0200

    Refactor: Variables.findSubstitute(): de Morgan

--- src/main/java/org/opennars/language/Variables.java
+++ src/main/java/org/opennars/language/Variables.java
@@ -221,7 +221,7 @@ public class Variables {
             //consider temporal order on term matching
             boolean isSameOrder = term1.getTemporalOrder() == term2.getTemporalOrder();
             boolean isSameSpatial = term1.getIsSpatial() == term2.getIsSpatial();
-            boolean spatialRENAMEME = !isSameOrder || !isSameSpatial;
+            boolean spatialRENAMEME = !(isSameOrder && isSameSpatial);
 
             if(term1 instanceof Conjunction && term2 instanceof Conjunction) {
                 if(spatialRENAMEME)
