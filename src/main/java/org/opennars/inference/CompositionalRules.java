0b5b21a7f673485d1ffd6da9ca0b7c2bb8c10116
PtrMan <robertw89@googlemail.com>
2018-05-20 02:55:17 +0200

    Refactor: CompositionalRules.decomposeStatement(): truth value calculation

--- src/main/java/org/opennars/inference/CompositionalRules.java
+++ src/main/java/org/opennars/inference/CompositionalRules.java
@@ -303,15 +303,8 @@ public final class CompositionalRules {
                 nal.doublePremiseTask(conj, truth, budget, false, false);
             }
         } else {
-            final TruthValue v1;
-            final TruthValue v2;
-            if (compoundTask) {
-                v1 = taskSentence.truth;
-                v2 = belief.truth;
-            } else {
-                v1 = belief.truth;
-                v2 = taskSentence.truth;
-            }
+            final TruthValue v1 = compoundTask ? taskSentence.truth : belief.truth;
+            final TruthValue v2 = compoundTask ? belief.truth : taskSentence.truth;
 
             if (compound instanceof Conjunction || compound instanceof Disjunction) {
                 if (taskSentence.isGoal() && !compoundTask) {
