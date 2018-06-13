474b43edac264e4ef972af37419cd59a517a9ce1
PtrMan <robertw89@googlemail.com>
2018-06-13 17:14:28 +0200

    Refactor: ConfigReader: ctor parameters

--- src/main/java/org/opennars/io/ConfigReader.java
+++ src/main/java/org/opennars/io/ConfigReader.java
@@ -1,6 +1,6 @@
 package org.opennars.io;
 
-import org.opennars.interfaces.Pluggable;
+import org.opennars.interfaces.pub.Reasoner;
 import org.opennars.main.NarParameters;
 import org.opennars.main.Parameters;
 import org.opennars.plugin.Plugin;
@@ -14,20 +14,23 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConfigReader {
-    public static void loadFrom(final String filepath, final Pluggable reasoner, final NarParameters parameters) throws IOException, IllegalAccessException, ParseException, ParserConfigurationException, SAXException, ClassNotFoundException, NoSuchMethodException, InstantiationException, InvocationTargetException {
+    public static void loadFrom(final String filepath, final Reasoner reasoner, final NarParameters parameters) throws IOException, IllegalAccessException, ParseException, ParserConfigurationException, SAXException, ClassNotFoundException, NoSuchMethodException, InstantiationException, InvocationTargetException {
         loadFromImpl(new File(filepath), reasoner, parameters);
     }
 
-    public static void loadFrom(final File file, final Pluggable reasoner, final NarParameters parameters) throws IOException, IllegalAccessException, ParseException, ParserConfigurationException, SAXException, ClassNotFoundException, NoSuchMethodException, InstantiationException, InvocationTargetException {
+    public static void loadFrom(final File file, final Reasoner reasoner, final NarParameters parameters) throws IOException, IllegalAccessException, ParseException, ParserConfigurationException, SAXException, ClassNotFoundException, NoSuchMethodException, InstantiationException, InvocationTargetException {
         loadFromImpl(file, reasoner, parameters);
     }
 
-    private static void loadFromImpl(final File file, final Pluggable reasoner, final NarParameters parameters) throws IOException, IllegalAccessException, ParseException, ParserConfigurationException, SAXException, ClassNotFoundException, NoSuchMethodException, InstantiationException, InvocationTargetException {
+    private static void loadFromImpl(final File file, final Reasoner reasoner, final NarParameters parameters) throws IOException, IllegalAccessException, ParseException, ParserConfigurationException, SAXException, ClassNotFoundException, NoSuchMethodException, InstantiationException, InvocationTargetException {
         final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         final DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
         final Document document = documentBuilder.parse(file);
@@ -56,13 +59,10 @@ public class ConfigReader {
                     }
 
                     final String pluginClassPath = iPlugin.getAttributes().getNamedItem("classpath").getNodeValue();
-                    final String arg0 = iPlugin.getAttributes().getNamedItem("arg0").getNodeValue();
-                    final String arg1 = iPlugin.getAttributes().getNamedItem("arg1").getNodeValue();
-                    final String arg2 = iPlugin.getAttributes().getNamedItem("arg2").getNodeValue();
-                    final String arg3 = iPlugin.getAttributes().getNamedItem("arg3").getNodeValue();
-                    final String arg4 = iPlugin.getAttributes().getNamedItem("arg4").getNodeValue();
 
-                    Plugin createdPlugin = createPluginByClassname(pluginClassPath, arg0, arg1, arg2, arg3, arg4);
+                    final NodeList arguments = iPlugin.getChildNodes();
+
+                    Plugin createdPlugin = createPluginByClassnameAndArguments(pluginClassPath, arguments, reasoner);
                     reasoner.addPlugin(createdPlugin);
                 }
             }
@@ -110,11 +110,65 @@ public class ConfigReader {
         }
     }
 
+    private static Plugin createPluginByClassnameAndArguments(String pluginClassPath, NodeList arguments, Reasoner reasoner) throws ParseException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        List<Class> types = new ArrayList<>();
+        List<Object> values = new ArrayList<>();
+
+        for (int parameterIdx = 0; parameterIdx < arguments.getLength(); parameterIdx++) {
+            final Node iParameter = arguments.item(parameterIdx);
+
+            if (iParameter.getNodeType() != Node.ELEMENT_NODE) {
+                continue;
+            }
+
+            String typeString = null;
+            String valueString = null;
+            final boolean specialIsReasoner = iParameter.getAttributes().getNamedItem("isReasoner") != null;
+
+            if (!specialIsReasoner) {
+                typeString = iParameter.getAttributes().getNamedItem("type").getNodeValue();
+                valueString = iParameter.getAttributes().getNamedItem("value").getNodeValue();
+            }
+
+            if (specialIsReasoner) {
+                types.add(Reasoner.class);
+                values.add(reasoner);
+            }
+            else if (typeString.equals("int.class")) {
+                types.add(int.class);
+                values.add(Integer.parseInt(valueString));
+            }
+            else if (typeString.equals("float.class")) {
+                types.add(float.class);
+                values.add(Float.parseFloat(valueString));
+            }
+            else if (typeString.equals("java.lang.String.class")) {
+                types.add(java.lang.String.class);
+                values.add(valueString);
+            }
+            else {
+                throw new ParseException("Unknown type", 0);
+            }
+        }
+
+        Class[] typesAsArr = types.toArray(new Class[types.size()]);
+        Object[] valuesAsArr = values.toArray(new Object[values.size()]);
+
+        Class c = Class.forName(pluginClassPath);
+
+        Plugin createdPlugin = (Plugin)c.getConstructor(typesAsArr).newInstance(valuesAsArr);
+        return createdPlugin;
+    }
+
     public static void main(String[] args) throws IOException, InstantiationException, InvocationTargetException, NoSuchMethodException, ParserConfigurationException, SAXException, IllegalAccessException, ParseException, ClassNotFoundException {
         NarParameters params = new NarParameters();
 
         loadFrom("../opennars/src/main/config/defaultConfig.xml", null, params);
     }
+
+
+
+
 
     /**
      *
