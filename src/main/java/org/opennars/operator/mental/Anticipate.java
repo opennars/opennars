4975b527b83e673b54d93649e7dc27f278352c72
PtrMan <robertw89@googlemail.com>
2018-06-17 22:24:04 +0200

    Refactor: Anticipate: rename + code motion

--- src/main/java/org/opennars/operator/mental/Anticipate.java
+++ src/main/java/org/opennars/operator/mental/Anticipate.java
@@ -41,7 +41,7 @@ import java.util.*;
  //*
 public class Anticipate extends Operator implements EventObserver {
 
-    public final Map<Vector2Int,LinkedHashSet<Term>> anticipations = new LinkedHashMap();
+    public final Map<Prediction,LinkedHashSet<Term>> anticipations = new LinkedHashMap();
             
     final Set<Term> newTasks = new LinkedHashSet();
     DerivationContext nal;
@@ -62,16 +62,7 @@ public class Anticipate extends Operator implements EventObserver {
                                                     BudgetFunctions.truthToQuality(expiredTruth), n.narParameters);
         return true;
     }
-    
-    class Vector2Int {
-        public final long predictionCreationTime; //2014 and this is still the best way to define a data structure that simple?
-        public final long predictedOccurenceTime;
-        public Vector2Int(final long predictionCreationTime, final long predictedOccurenceTime) { //rest of the crap:
-            this.predictionCreationTime=predictionCreationTime; //when the prediction happened
-            this.predictedOccurenceTime=predictedOccurenceTime; //when the event is expected
-        }
-    }
-    
+
     public void updateAnticipations() {
 
         if (anticipations.isEmpty()) return;
@@ -82,10 +73,10 @@ public class Anticipate extends Operator implements EventObserver {
         
         boolean hasNewTasks = !newTasks.isEmpty();
         
-        final Iterator<Map.Entry<Vector2Int, LinkedHashSet<Term>>> aei = anticipations.entrySet().iterator();
+        final Iterator<Map.Entry<Prediction, LinkedHashSet<Term>>> aei = anticipations.entrySet().iterator();
         while (aei.hasNext()) {
             
-            final Map.Entry<Vector2Int, LinkedHashSet<Term>> ae = aei.next();
+            final Map.Entry<Prediction, LinkedHashSet<Term>> ae = aei.next();
             
             final long aTime = ae.getKey().predictedOccurenceTime;
             final long predictionstarted=ae.getKey().predictionCreationTime;
@@ -219,7 +210,7 @@ public class Anticipate extends Operator implements EventObserver {
        }
         
         final LinkedHashSet<Term> ae = new LinkedHashSet();
-        anticipations.put(new Vector2Int(memory.time(),occurenceTime), ae);
+        anticipations.put(new Prediction(memory.time(),occurenceTime), ae);
 
         ae.add(content);
         anticipationFeedback(content, t, memory);
@@ -274,5 +265,14 @@ public class Anticipate extends Operator implements EventObserver {
         nal.derivedTask(task, false, true, false); 
         task.setElemOfSequenceBuffer(true);
         nal.memory.emit(DISAPPOINT.class, task);
+    }
+
+    class Prediction {
+        public final long predictionCreationTime; //2014 and this is still the best way to define a data structure that simple?
+        public final long predictedOccurenceTime;
+        public Prediction(final long predictionCreationTime, final long predictedOccurenceTime) { //rest of the crap:
+            this.predictionCreationTime=predictionCreationTime; //when the prediction happened
+            this.predictedOccurenceTime=predictedOccurenceTime; //when the event is expected
+        }
     }
 }
