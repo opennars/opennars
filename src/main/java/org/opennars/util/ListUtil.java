9a760d85ff117d85e16dabd6e3d614342b627093
PtrMan <robertw89@googlemail.com>
2018-08-01 04:04:10 +0200

    Add: ListUtil.findAny()

--- src/main/java/org/opennars/util/ListUtil.java
+++ src/main/java/org/opennars/util/ListUtil.java
@@ -4,8 +4,16 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class ListUtil {
-    public static<T> T findAny(final List<T> l, final Predicate<T> predicate) {
-        for( final T i : l) {
+    /**
+     * tries to select the first element where the predicate matches from front (index 0) to the end of the list
+     *
+     * @param candidates the candidates from which the method may select the first one
+     * @param predicate the checked predicate for each element
+     * @param <T> generic type
+     * @return element which matched first, null if none matched
+     */
+    public static<T> T findAny(final List<T> candidates, final Predicate<T> predicate) {
+        for( final T i : candidates) {
             if(predicate.test(i)) {
                 return i;
             }
