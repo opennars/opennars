c43457784d929f3ec8ebbaaeb1f67b6c1ace2510
PtrMan <robertw89@googlemail.com>
2018-08-01 15:59:54 +0200

    Refactor: adder Seralizable

--- src/main/java/org/opennars/interfaces/conceptProcessing/ProcessGoal.java
+++ src/main/java/org/opennars/interfaces/conceptProcessing/ProcessGoal.java
@@ -1,9 +1,25 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.opennars.interfaces.conceptProcessing;
+
+import java.io.Serializable;
 
 /**
  * Used to implement the (internal) goal processing
  */
-public interface ProcessGoal extends Process {
+public interface ProcessGoal extends Process, Serializable {
     /*
      * To accept a new goal, and check for revisions and realization, then
      * decide whether to actively pursue it, potentially executing in case of an operation goal
--- src/main/java/org/opennars/interfaces/conceptProcessing/ProcessJudgment.java
+++ src/main/java/org/opennars/interfaces/conceptProcessing/ProcessJudgment.java
@@ -1,9 +1,25 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.opennars.interfaces.conceptProcessing;
+
+import java.io.Serializable;
 
 /**
  * Used to implement the (internal) judgment processing
  */
-public interface ProcessJudgment extends Process {
+public interface ProcessJudgment extends Process, Serializable {
     /*
      * To accept a new judgment as belief, and check for revisions and solutions.
      * Revisions will be processed as judgment tasks by themselves.
