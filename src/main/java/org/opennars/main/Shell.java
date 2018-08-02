46931f42a1b81cd885ccf328674da34843d83525
PtrMan <robertw89@googlemail.com>
2018-08-02 17:06:48 +0200

    reverted Shell

--- src/main/java/org/opennars/main/Shell.java
+++ src/main/java/org/opennars/main/Shell.java
@@ -3,20 +3,6 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-/**
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
  * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -34,7 +20,6 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
-import java.net.URISyntaxException;
 import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -55,15 +40,6 @@ public class Shell {
      * @param args command line arguments
      */
     public static void main(final String[] args) throws IOException, InstantiationException, InvocationTargetException, NoSuchMethodException, ParserConfigurationException, IllegalAccessException, SAXException, ClassNotFoundException, ParseException {
-        // just for debugging the classpath issue which is related to the issue that we can't load the configuration
-        ClassLoader loader = Shell.class.getClassLoader();
-        System.out.println(loader.getResource("org/opennars/main/Shell.class"));
-
-        try {
-            System.out.println(Shell.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-        } catch (URISyntaxException e) {
-        }
-
         final Shell nars = new Shell(new Nar());
         nars.run(args);
     }
