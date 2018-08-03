0858af988a36280690c3ebc84ad9e3b5c378b470
PtrMan <robertw89@googlemail.com>
2018-08-03 04:21:35 +0200

    Refactor: CompositionalRules.introVarOuter(): formating

--- src/main/java/org/opennars/inference/CompositionalRules.java
+++ src/main/java/org/opennars/inference/CompositionalRules.java
@@ -388,18 +388,13 @@ public final class CompositionalRules {
             if (commonTerm != null) {
                 subs.put(commonTerm, varInd2);
 
-
                 if (index == 0) {
-
-                        term12 = ((CompoundTerm) term12).applySubstitute(subs);
-                        term22 = applySubstituteIfCompoundTerm(varInd2, term22, subs);
-
+                    term12 = ((CompoundTerm) term12).applySubstitute(subs);
+                    term22 = applySubstituteIfCompoundTerm(varInd2, term22, subs);
                 }
                 else {
-
-                        term21 = ((CompoundTerm) term21).applySubstitute(subs);
-                        term11 = applySubstituteIfCompoundTerm(varInd2, term11, subs);
-
+                    term21 = ((CompoundTerm) term21).applySubstitute(subs);
+                    term11 = applySubstituteIfCompoundTerm(varInd2, term11, subs);
                 }
             }
         }
