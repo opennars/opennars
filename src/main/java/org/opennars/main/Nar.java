9b85ecde8b6789784f0939fd52aefdbaf9697bdb
PtrMan <robertw89@googlemail.com>
2019-03-13 20:39:44 +0100

    Update: Reasoner: split name and version into two strings

--- src/main/java/org/opennars/main/Nar.java
+++ src/main/java/org/opennars/main/Nar.java
@@ -82,9 +82,14 @@ public class Nar extends SensoryChannel implements Reasoner, Serializable, Runna
     private Long cycle = new Long(0);
 
     /**
-     * The information about the version and date of the project.
+     * The information about the version of the project
      */
-    public static final String VERSION = "Open-NARS v3.0.2";
+    public static final String VERSION = "v3.0.2";
+
+    /**
+     * Name of the reasoner of the project
+     */
+    public static final String REASONERNAME = "Open-NARS";
 
     /**
      * The project web sites.
