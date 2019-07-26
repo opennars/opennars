0d710276f53ab59233e78e91a222dc9f73649b8a
PtrMan <robertw89@googlemail.com>
2019-07-26 23:03:27 +0200

    Refactor: code style

--- src/test/java/org/opennars/core/ParallelizedParameterized.java
+++ src/test/java/org/opennars/core/ParallelizedParameterized.java
@@ -9,40 +9,33 @@ import java.util.concurrent.TimeUnit;
 
 // from http://hwellmann.blogspot.com/2009/12/running-parameterized-junit-tests-in.html
 public class ParallelizedParameterized extends Parameterized {
-    private static class ThreadPoolScheduler implements RunnerScheduler
-    {
+    private static class ThreadPoolScheduler implements RunnerScheduler {
         private ExecutorService executor;
 
-        public ThreadPoolScheduler()
-        {
+        public ThreadPoolScheduler() {
             String threads = System.getProperty("junit.parallel.threads", "6");
             int numThreads = Integer.parseInt(threads);
             executor = Executors.newFixedThreadPool(numThreads);
         }
 
         @Override
-        public void finished()
-        {
+        public void finished() {
             executor.shutdown();
-            try
-            {
+            try {
                 executor.awaitTermination(10, TimeUnit.MINUTES);
             }
-            catch (InterruptedException exc)
-            {
+            catch (InterruptedException exc) {
                 throw new RuntimeException(exc);
             }
         }
 
         @Override
-        public void schedule(Runnable childStatement)
-        {
+        public void schedule(Runnable childStatement) {
             executor.submit(childStatement);
         }
     }
 
-    public ParallelizedParameterized(Class klass) throws Throwable
-    {
+    public ParallelizedParameterized(Class klass) throws Throwable {
         super(klass);
         setScheduler(new ThreadPoolScheduler());
     }
